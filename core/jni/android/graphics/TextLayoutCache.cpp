0ecfb035ca623374aa1c324c7ab33f33a3d48c54
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2011-12-14 15:51:42 +0059

    Fix aliasing and C++11 issues.
    frameworks/base: Fix various aliasing violations
    
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
    
    frameworks/base: Fix aliasing violations
    
    Fix violations of strict aliasing rules
    
    Change-Id: Ia83db6a0be18f52e8f274c581c3b42f031b39247
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
    
    frameworks/base: Fix build in ISO C++11 mode
    
    Fix various incompatibilities with ISO C++11:
    - Assumption that char16_t is defined to uint16_t
    - constexpr usage
    - macro constructs that could be interpreted as string literals
    - enum typesafety
    
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
    Signed-off-by: Evan McClain <aeroevan@gmail.com>
    
    Conflicts:
    	cmds/stagefright/SineSource.h
    	core/jni/android/graphics/TextLayoutCache.cpp
    	core/jni/android_util_Binder.cpp
    	core/jni/android_view_KeyCharacterMap.cpp
    	libs/gui/SurfaceTexture.cpp
    	media/libstagefright/OMXCodec.cpp
    	services/audioflinger/AudioResampler.h
    	core/jni/android_server_BluetoothEventLoop.cpp
    
    Change-Id: I185b97f07e89bed93b92546bc0fdf895bd0cc92d
    
    Conflicts:
    
    	core/jni/android/graphics/TextLayoutCache.cpp
    	core/jni/android/graphics/TextLayoutCache.h
    	core/jni/android_os_FileUtils.cpp
    	core/jni/android_server_BluetoothEventLoop.cpp
    	core/jni/android_view_Surface.cpp
    
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>
    Change-Id: If0a59e0bd6a89025dd7e0a0efaaf8d55a952ef34

--- core/jni/android/graphics/TextLayoutCache.cpp
+++ core/jni/android/graphics/TextLayoutCache.cpp
@@ -144,6 +144,7 @@ sp<TextLayoutValue> TextLayoutCache::getValue(const SkPaint* paint,
                     "This indicates that the cache already has an entry with the "
                     "same key but it should not since we checked earlier!"
                     " - start = %d, count = %d, contextCount = %d - Text = '%s'",
+
                     start, count, contextCount, String8(key.getText() + start, count).string());
 
             if (mDebugEnabled) {
@@ -155,7 +156,9 @@ sp<TextLayoutValue> TextLayoutCache::getValue(const SkPaint* paint,
                         value.get(), start, count, contextCount, size, mMaxSize - mSize,
                         value->getElapsedTime() * 0.000001f,
                         (totalTime - value->getElapsedTime()) * 0.000001f,
+
                         String8(key.getText() + start, count).string());
+
             }
         } else {
             if (mDebugEnabled) {
@@ -165,7 +168,9 @@ sp<TextLayoutValue> TextLayoutCache::getValue(const SkPaint* paint,
                         " - Compute time %0.6f ms - Text = '%s'",
                         start, count, contextCount, size, mMaxSize - mSize,
                         value->getElapsedTime() * 0.000001f,
+
                         String8(key.getText() + start, count).string());
+
             }
         }
     } else {
@@ -185,7 +190,9 @@ sp<TextLayoutValue> TextLayoutCache::getValue(const SkPaint* paint,
                         value->getElapsedTime() * 0.000001f,
                         elapsedTimeThruCacheGet * 0.000001f,
                         deltaPercent,
+
                         String8(key.getText() + start, count).string());
+
             }
             if (mCacheHitCount % DEFAULT_DUMP_STATS_CACHE_HIT_INTERVAL == 0) {
                 dumpCacheStats();
@@ -256,6 +263,7 @@ TextLayoutCacheKey::TextLayoutCacheKey(const TextLayoutCacheKey& other) :
         hinting(other.hinting),
         variant(other.variant),
         language(other.language) {
+
 }
 
 int TextLayoutCacheKey::compare(const TextLayoutCacheKey& lhs, const TextLayoutCacheKey& rhs) {
@@ -296,6 +304,7 @@ int TextLayoutCacheKey::compare(const TextLayoutCacheKey& lhs, const TextLayoutC
     if (lhs.language > rhs.language) return +1;
 
     return memcmp(lhs.getText(), rhs.getText(), lhs.contextCount * sizeof(UChar));
+
 }
 
 size_t TextLayoutCacheKey::getSize() const {
@@ -420,7 +429,7 @@ void TextLayoutShaper::computeValues(const SkPaint* paint, const UChar* chars,
                     } else if (!U_SUCCESS(status) || rc < 1) {
                         ALOGW("Need to force to single run -- string = '%s',"
                                 " status = %d, rc = %d",
-                                String8(chars + start, count).string(), status, int(rc));
+                                String8((const char16_t*)chars + start, count).string(), status, int(rc));
                         isRTL = (paraDir == 1);
                         useSingleRun = true;
                     } else {
@@ -945,7 +954,7 @@ sp<TextLayoutValue> TextLayoutEngine::getValue(const SkPaint* paint, const jchar
             contextCount, dirFlags);
     if (value == NULL) {
         ALOGE("Cannot get TextLayoutCache value for text = '%s'",
-                String8(text + start, count).string());
+                String8((const char16_t*)text + start, count).string());
     }
 #else
     value = new TextLayoutValue(count);
