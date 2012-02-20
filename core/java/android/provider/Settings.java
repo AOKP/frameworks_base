0ea02c155f4ad12f4b32227b33f184ae2f4e4613
Roman Birg <romanbirg@gmail.com>
2012-02-19 23:01:14 -0600

    SystemUI: add signal text (1/2) WIP
    Change-Id: I0b1a246f4744e71fb6cebfdfe2f674958971048e
    
    SystemUI: replace signal bars with signal text if option chosen
    
    Change-Id: I3b50747031f6e80edcad4499f0ad2646483c94c0
    
    fix airplane mode with SignalText
    Took the visibilty checks out of SignalText.java and moved to signalClusterView.
    Airplane mode icon now properly responds.
    
    Change-Id: I4e97854a3ed59df26d12b881b73b8f59c75de3db
    
    fix Dbm signal readout for LTE/4g
    This uses deprecated methods for getting signal values, but
    it is the way it's currently being done in Settings.apk in AOSP.
    there is no LTE functionality in the new preferred method.  Until
    such time as LTE signals are exposed, this looks to be the only way.
    
    Clean up
    I forgot to take out the log statements and delete
    commented out code.
    
    Change-Id: I96fe28fdf946e1372ec4dd8df3adae4aece4b509
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6807,6 +6807,27 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
 
+        /**
+         * Whether to show statusbar signal text
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_SIGNAL_TEXT = "statusbar_signal_text";
+
+        /**
+         * statusbar signal text color
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_SIGNAL_TEXT_COLOR = "statusbar_signal_text_color";
+
+        /**
+         * whether to hide the signal barss
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_HIDE_SIGNAL_BARS = "statusbar_hide_signal_bars";
+
     }
 
     /**
@@ -7024,3 +7045,4 @@ public final class Settings {
         return "android-" + Long.toHexString(androidId);
     }
 }
+
