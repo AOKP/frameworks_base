07847682b44c3456a617451b0d1b4987ded80bac
Jim Huang <jserv@0xlab.org>
2012-03-23 16:26:17 +0800

    audioflinger: Optimze ditherAndClamp with ARM NEON
    Change-Id: Ifc436e39f15b103cec404e8a81fa8b8a6b5f0e5f

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -30,4 +30,12 @@ LOCAL_STATIC_LIBRARIES := \
 
 LOCAL_MODULE:= libaudioflinger
 
+ifeq ($(BOARD_USE_MOTO_DOCK_HACK),true)
+   LOCAL_CFLAGS += -DMOTO_DOCK_HACK
+endif
+
+ifeq ($(ARCH_ARM_HAVE_NEON),true)	
+   LOCAL_CFLAGS += -D__ARM_HAVE_NEON
+endif
+
 include $(BUILD_SHARED_LIBRARY)
