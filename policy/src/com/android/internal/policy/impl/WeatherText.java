25bbca3164e0cf375e3b1d04ee95756cdafac7a9
sethyx <sethyx@gmail.com>
2012-08-16 16:08:07 +0200

    Port lockscreen info-lines from ICS (1/2)
    Don't merge this commit yet, needs testing on all of the devices b/c of the different layouts.
    
    - Restructured lockscreen layout: keyguard_screen_info.xml contains the informational elements, this should be included on each layout (except in sw600dp)
    - Weather: added back text option
    - Calendar (upcoming events): added back w/ all of the old features
    - Cleaned up parts of code, calendar should be checked further
    
    Wipe highly recommended!
    
    Change-Id: I43429db7a85d22d41f422c189f0383c1f1143ef9
    Signed-off-by: sethyx <sethyx@gmail.com>

---
+++ policy/src/com/android/internal/policy/impl/WeatherText.java
@@ -0,0 +1,74 @@
+package com.android.internal.policy.impl;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class WeatherText extends TextView {
+
+    private boolean mAttached;
+
+    public static final String EXTRA_CITY = "city";
+    public static final String EXTRA_CONDITION = "condition";
+    public static final String EXTRA_FORECAST_DATE = "forecast_date";
+    public static final String EXTRA_TEMP = "temp";
+    public static final String EXTRA_HUMIDITY = "humidity";
+    public static final String EXTRA_WIND = "wind";
+    public static final String EXTRA_LOW = "todays_low";
+    public static final String EXTRA_HIGH = "todays_high";
+    
+    private static boolean showLocation = false;
+    
+
+    BroadcastReceiver weatherReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String wText = (showLocation) ? (intent.getCharSequenceExtra(EXTRA_CITY) + ", " + intent.getCharSequenceExtra(EXTRA_TEMP) + ", "
+                    + intent.getCharSequenceExtra(EXTRA_CONDITION)) : (intent.getCharSequenceExtra(EXTRA_TEMP) + ", "
+                            + intent.getCharSequenceExtra(EXTRA_CONDITION));
+            setText(wText);
+        }
+    };
+
+    public WeatherText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setText("");
+        ContentResolver resolver = mContext.getContentResolver();
+        showLocation = Settings.System.getInt(resolver, Settings.System.WEATHER_SHOW_LOCATION, 0) == 1;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (!mAttached) {
+            mAttached = true;
+            IntentFilter filter = new IntentFilter("com.aokp.romcontrol.INTENT_WEATHER_UPDATE");
+            getContext().registerReceiver(weatherReceiver, filter, null, getHandler());
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mAttached) {
+            getContext().unregisterReceiver(weatherReceiver);
+            mAttached = false;
+        }
+    }
+
+    public void updateWeather(Intent intent) {
+        String wText = (showLocation) ? (intent.getCharSequenceExtra(EXTRA_CITY) + ", " + intent.getCharSequenceExtra(EXTRA_TEMP) + ", "
+                + intent.getCharSequenceExtra(EXTRA_CONDITION)) : (intent.getCharSequenceExtra(EXTRA_TEMP) + ", "
+                        + intent.getCharSequenceExtra(EXTRA_CONDITION));
+        setText(wText);
+    }
+}
