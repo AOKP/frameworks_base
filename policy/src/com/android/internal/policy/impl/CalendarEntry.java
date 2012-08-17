350331a4e2ca5b02fa79b1e6481d90070b8a7361
sethyx <sethyx@gmail.com>
2012-08-17 19:32:58 +0200

    Port lockscreen info-lines from ICS (1/2)
    Don't merge this commit yet, needs testing on all of the devices b/c of the different layouts.
    
    - Restructured lockscreen layout: keyguard_screen_info.xml contains the informational elements, this should be included on each layout (except in sw600dp)
    - Weather: added back text option
    - Calendar (upcoming events): added back w/ all of the old features
    - Cleaned up parts of code, calendar should be checked further
    
    PS2: Rebased, amended color-switcher functions
    
    Change-Id: I43429db7a85d22d41f422c189f0383c1f1143ef9
    Signed-off-by: sethyx <sethyx@gmail.com>

---
+++ policy/src/com/android/internal/policy/impl/CalendarEntry.java
@@ -0,0 +1,34 @@
+package com.android.internal.policy.impl;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.R;
+
+public class CalendarEntry extends LinearLayout {
+
+    private static final String TAG = "CalendarEntry";
+    TextView mTitle;
+    TextView mDetails;
+
+    public CalendarEntry(Context context, String title, String details, int width) {
+        super(context);
+        View view = LayoutInflater.from(context).inflate(R.layout.calendar_entry, this);
+
+        this.mTitle = (TextView) view.findViewById(R.id.event_title);
+        this.mDetails = (TextView) view.findViewById(R.id.event_details);
+        mTitle.setWidth(width);
+        mTitle.setText(title);
+        mDetails.setText(details);
+    }
+
+    public void setColor(int color) {
+        mTitle.setTextColor(color);
+        mDetails.setTextColor(color);
+    }
+}
+
