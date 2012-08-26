3563a6c91034846eb45e2f48baba090a185ac837
BMc08GT <bmc08gt.teamb00sted@encounterpc.com>
2012-08-26 04:26:53 -0400

    Add ability to unlink notification volume from ringtone volume (1/2)
    Original code by romanbb from ICS
    
    Change-Id: I8276e299cbc0c5b4cbad986f06c89fec4dcf2730

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -668,7 +668,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                 Settings.System.MODE_RINGER_STREAMS_AFFECTED, mRingerModeAffectedStreams);
 
         mLinkNotificationWithVolume = Settings.System.getInt(cr,
-                Settings.System.VOLUME_LINK_NOTIFICATION, 1) == 1;
+                Settings.System.VOLUME_LINK_NOTIFICATION, true);
 
         mMuteAffectedStreams = System.getInt(cr,
                 System.MUTE_STREAMS_AFFECTED,
@@ -3193,7 +3193,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                 }
 
                 mLinkNotificationWithVolume = Settings.System.getInt(mContentResolver,
-                        Settings.System.VOLUME_LINK_NOTIFICATION, 1) == 1;
+                        Settings.System.VOLUME_LINK_NOTIFICATION, true);
                 if (mLinkNotificationWithVolume) {
                     mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_RING;
                 } else {
