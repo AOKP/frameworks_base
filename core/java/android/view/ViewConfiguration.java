b4ce1fda2ccb45a496b2b5fee3ad9ef1c0bfe2cd
xkonni <konstantin.koslowski@gmail.com>
2012-09-04 22:49:48 -0400

    Hardware Key remapping (Part 2 of 2)
    Ported from Cynanogenmod (cm10)
    
    Added Framework portion - Part 1 contains Romcontrol additions
    
    Original from Konstantin Koslowski (http://r.cyanogenmod.com/#/c/20139/)
    
    Patch Set 2 : Spacing cleanup / Notes update for original author/link
    Patch Set 3 :  add author to commit
    Patch Set 4 :  add missing information to PhoneWindowManager.java
    Patch Set 5 :  add author to commit
    Patch Set 6 :  Rebase and set default for config_deviceHardwareKeys to 0
    Patch Set 7 :  Remove Menu UI Overflow
    
    Signed-off-by: Desterly <d3s7royer@gmail.com>
    Signed-off-by: BMc08GT <bmc08gt.teamb00sted@gmail.com>
    Change-Id: Id0b800533dd03e26abfb65b97b1ab5911e7e6060

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -692,7 +692,7 @@ public class ViewConfiguration {
      * @return true if a permanent menu key is present, false otherwise.
      */
     public boolean hasPermanentMenuKey() {
-        return sHasPermanentMenuKey;
+            return sHasPermanentMenuKey;
     }
 
     /**
