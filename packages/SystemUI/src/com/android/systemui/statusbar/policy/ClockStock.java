0736232b84cbc949fe5e5d2ffa473696f375e220
Stevespear426 <stephen.k.spear@gmail.com>
2012-10-24 19:56:16 -0400

    Statusbar: Adds clickable actions for clock and date
    Working as intended for now
    Clock click alarms, long click google now voice actions
    
    Date click today in calendar, long click new event calendar
    
    my intent was to make accessing things like caledars, alarms, and
    Gnow easier from the notification panel since thats where I generally
    get notified if i need to add/ change one of those things.
    
    I need to add Custom target options. Cuz Mike dont let me gets lazy..
    
    Change-Id: I97b2caf75060e41ff5ebd83964d9bb310b5cdd13

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/ClockStock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/ClockStock.java
@@ -16,6 +16,8 @@
 
 package com.android.systemui.statusbar.policy;
 
+import android.app.ActivityManagerNative;
+import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -25,6 +27,9 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.provider.AlarmClock;
+import android.provider.Settings;
+import android.speech.RecognizerIntent;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.format.DateFormat;
@@ -35,6 +40,8 @@ import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -47,7 +54,7 @@ import com.android.internal.R;
  * This widget display an analogic clock with two hands for hours and
  * minutes.
  */
-public class ClockStock extends TextView {
+public class ClockStock extends TextView implements OnClickListener, OnLongClickListener {	
     private boolean mAttached;
     private Calendar mCalendar;
     private String mClockFormatString;
@@ -69,6 +76,10 @@ public class ClockStock extends TextView {
 
     public ClockStock(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+         if(isClickable()){
+            setOnClickListener(this);
+            setOnLongClickListener(this);
+        }
     }
 
     @Override
@@ -204,5 +215,31 @@ public class ClockStock extends TextView {
         return result;
 
     }
+        @Override
+        public void onClick(View v) {
+          StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(Context.STATUS_BAR_SERVICE);
+            try {
+                ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+            } catch (Exception e) {
+            }
+	        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intent);
+            statusBarManager.collapse();
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+          StatusBarManager statusBarManager = (StatusBarManager) getContext().getSystemService(Context.STATUS_BAR_SERVICE);
+            try {
+                ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
+            } catch (Exception e) {
+            }
+	        Intent intent = new Intent(RecognizerIntent.ACTION_WEB_SEARCH);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intent);
+            statusBarManager.collapse();
+            return true;
+        }
 }
 
