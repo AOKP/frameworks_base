1fc1c32ce0db085fb664727a3d6e57100311ab58
ihtfp69 <ihtfp69@gmail.com>
2012-11-06 17:03:24 -0500

    TextView: Allow copy/paste in Google Talk
    Change-Id: I81b4c7b0449e5a965b5472731ec6a341780d848b

--- core/java/android/widget/TextView.java
+++ core/java/android/widget/TextView.java
@@ -872,6 +872,10 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
             }
         }
         a.recycle();
+        // if the TextView is from Talk and autoLink is set to 'all' then make the text selectable
+        if(getContext().getPackageName().equals("com.google.android.talk") && mAutoLinkMask==0x0f){
+            setTextIsSelectable(true);
+        }
 
         BufferType bufferType = BufferType.EDITABLE;
 
