2fa85c14c4785c6f2e0a7d8147e5835a9d18c27c
Zaphod <mwilson@acesappraisal.com>
2012-11-12 06:47:09 -0500

    SystemUI: Create Extensible Target Launcher [WIP]
    This is the start of an object meant to handle the launching
    of custom targets and actions.  Right now, each 'area' (NavBar,
    NavRing, Lockscreen) all use similar, but replicated code.  When
    new Actions are created, they have to be coded into all areas.
    This will consolidate those actions into one place, making it
    easier to access and extend.
    
    I also moved some of our AOKP Actions/Toggles into this package.
    We will want to expand on this area as well.
    
    2:  Whitespace cleanup
    3:  Extended the Icon & Name getters.
    4:  Re-write NavBar to use AokpTarget.  Rename aokpTarget to
    AokpTarget for JBirdVegas :)  I also moved the infation of the NavBar
    keys to an xml layout.  I think this will allow us to better control
    the spacing & padding of icons without resorting to complicated padding
    formulas.
    5:  a few derp fixes and adjust the KeyButton Layouts.
    Change-Id: I22e5a08f513f180635008d6118109283165aa1b1
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>
    
    derp
    
    Change-Id: I87e8ff97306da9627c93e30be5717bc92dd1a0bf
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>

--- packages/SystemUI/src/com/android/systemui/aokp/LockscreenUnlock.java
+++ packages/SystemUI/src/com/android/systemui/aokp/LockscreenUnlock.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package com.android.systemui;
+package com.android.systemui.aokp;
 
 import android.os.Bundle;
 import android.app.Activity;
--- packages/SystemUI/src/com/android/systemui/aokp/RingVibSilentToggle.java
+++ packages/SystemUI/src/com/android/systemui/aokp/RingVibSilentToggle.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.systemui;
+package com.android.systemui.aokp;
 
 import android.app.Activity;
 import android.content.Context;
