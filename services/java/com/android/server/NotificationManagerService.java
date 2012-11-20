70470397596d22bfcba9c2e68e503d52f5c87624
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-20 21:17:17 +0000

    Squashed commit of the theme engine support.
    Updated 4.2 support for the old T-Mobile theme engine, as usual
    needs the provider and switcher apps installed as well.
    
    I'm finally dropping the 400+ commit history on this, since everybody
    else keeps picking it up from CM as a single patch anyway... But
    for the record, nothing of this would be possible without Josh, Ed,
    and the rest of the TMo guys who wrote and maintained it until 2.2:
    
    amit chabra <amit.chabra@t-mobile.com>
    Amit Kohli <amit.kohli@t-mobile.com>
    Chris Cogar <chriscogar@t-mobile.com>
    Dirk Sigurdson <dirk.sigurdson@t-mobile.com>
    Ed Carrigan <edward.carrigan@t-mobile.com>
    Gaurav Sharma <gaurav.sharma3@t-mobile.com>
    Hui Feng <hui.feng@t-mobile.com>
    John Ritz <john.ritz1@t-mobile.com>
    Josh Guilfoyle <josh.guilfoyle@t-mobile.com>
    Mark Roberts <mark.roberts48@t-mobile.com>
    Pankaj Kumar <Pankaj.kumar6@t-mobile.com>
    Samuel Cheung <samuel.cheung@t-mobile.com>
    Sergey Ten <sergey.ten6@t-mobile.com>
    
    Change-Id: I7148d51be48f28a2dc4bdf9ec9018f04b268ffc4
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>
    
    Conflicts:
    	core/jni/AndroidRuntime.cpp
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    	services/java/com/android/server/NotificationManagerService.java
    	services/java/com/android/server/SystemServer.java
    	services/java/com/android/server/am/ActivityManagerService.java
    	services/java/com/android/server/wm/WindowManagerService.java

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -82,6 +82,9 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
+
+import com.android.internal.app.ThemeUtils;
+
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -135,6 +138,7 @@ public class NotificationManagerService extends INotificationManager.Stub
     private static final String ENABLED_NOTIFICATION_LISTENERS_SEPARATOR = ":";
 
     final Context mContext;
+    Context mUiContext;
     final IActivityManager mAm;
     final UserManager mUserManager;
     final IBinder mForegroundToken = new Binder();
@@ -1132,6 +1136,13 @@ public class NotificationManagerService extends INotificationManager.Stub
         }
     };
 
+    private BroadcastReceiver mThemeChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mUiContext = null;
+        }
+    };
+
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -1357,6 +1368,7 @@ public class NotificationManagerService extends INotificationManager.Stub
 
         mSettingsObserver = new SettingsObserver(mHandler);
         mSettingsObserver.observe();
+        ThemeUtils.registerThemeChangeReceiver(mContext, mThemeChangeReceiver);
 
         // spin up NotificationScorers
         String[] notificationScorerNames = resources.getStringArray(
@@ -2281,6 +2293,13 @@ public class NotificationManagerService extends INotificationManager.Stub
             }
         }
         return -1;
+    }
+
+    private Context getUiContext() {
+        if (mUiContext == null) {
+            mUiContext = ThemeUtils.createUiContext(mContext);
+        }
+        return mUiContext != null ? mUiContext : mContext;
     }
 
     private void updateNotificationPulse() {
