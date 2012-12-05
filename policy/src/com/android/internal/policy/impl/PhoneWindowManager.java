5d3fe2f56cb963da5dac92851a8df0e2773d55b6
Zaphod <mwilson@acesappraisal.com>
2012-12-04 21:56:46 -0500

    FW - User select Tablet Mode [2/2] [WIP - DO NOT MERGE]
    Allows user to set UI to one of three modes:
    
    Phone/Tablet/Phablet.
    
    Needs RC commit as well.
    
    Patch 2:  Still having lots of resource issues.  I am just uploading
    	this to get a good diff look at what's changed in TabletStatusBar.
    
    Patch3:  Cleanup
    
    Patch 4:  This one at least boots and enables the tablet bar.
    	there is a lot of work still to be done to enable the quick
    	settings panel, etc.
    patch 5:  Implemented the Quick Settings framework.  Haven't tested yet
    	(Well, it compiled) - just posting in case I get busy again,
    	at least someone can take off where I've stopped.
    patch 6: Fix Setting/Notification toggle.
    
    patch 7: Tweak layouts - fix battery/clock
    
    patch 8: Shift Tablet NavRing to left corner
    
    patch 9: add drawables needed for patch 8
    
    patch 10: fix more layouts - various modes now working well
    
    Change-Id: Ia8d8a7ecaaca232587a54c9f7674936de7b05bda
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -274,6 +274,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mUserNavBarHeight;
     int mUserNavBarHeightLand;
     int mUserNavBarWidth;
+    int mUserUIMode; //User selected UI Mode (Phablet/Tablet, etc)
+    int mStockUIMode; // UI Mode as dictated by screen size.
 
     WindowState mKeyguard = null;
     KeyguardViewMediator mKeyguardMediator;
@@ -576,6 +578,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.System.NAVIGATION_BAR_HEIGHT_LANDSCAPE), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_WIDTH), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.USER_UI_MODE), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     "fancy_rotation_anim"), false, this,
                     UserHandle.USER_ALL);
@@ -1058,19 +1062,33 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         int shortSizeDp = shortSize * DisplayMetrics.DENSITY_DEFAULT / density;
 
         if (shortSizeDp < 600) {
-            // 0-599dp: "phone" UI with a separate status & navigation bar
-            mHasSystemNavBar = false;
-            mNavigationBarCanMove = true;
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.CURRENT_UI_MODE, 0);
+            mStockUIMode = 0; // Phone Mode
         } else if (shortSizeDp < 720) {
-            // 600+dp: "phone" UI with modifications for larger screens
-            mHasSystemNavBar = false;
-            mNavigationBarCanMove = false;
-            Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.CURRENT_UI_MODE, 2);
+            mStockUIMode = 2; // Phablet Mode
+        } else {
+            mStockUIMode = 1; // Tablet Mode (Combined SystemBar)
         }
-
+        mUserUIMode = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.USER_UI_MODE,mStockUIMode);
+        switch (mUserUIMode) {
+            case 0 :
+                // 0-599dp: "phone" UI with a separate status & navigation bar
+                mHasSystemNavBar = false;
+                mNavigationBarCanMove = true;
+                break;
+            case 1 :
+                // 720dp: "tablet" UI with a single combined status & navigation bar
+                mHasSystemNavBar = true;
+                mNavigationBarCanMove = false;
+                break;
+            case 2 :
+                // 600+dp: "phone" UI with modifications for larger screens
+                mHasSystemNavBar = false;
+                mNavigationBarCanMove = false;
+                break;
+        }
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.CURRENT_UI_MODE, mUserUIMode);
         if (!mHasSystemNavBar) {
              final boolean showByDefault = mContext.getResources().getBoolean(
                      com.android.internal.R.bool.config_showNavigationBar);
@@ -1146,10 +1164,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     public void updateSettings() {
-        DisplayMetrics metrics = new DisplayMetrics();
-        WindowManager wm = (WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE);
-        wm.getDefaultDisplay().getMetrics(metrics);
-        int density = metrics.densityDpi;
         ContentResolver resolver = mContext.getContentResolver();
         boolean updateRotation = false;
         synchronized (mLock) {
@@ -1227,14 +1241,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mUserNavBarHeight = NavHeight;
             mUserNavBarHeightLand = NavHeightLand;
             mUserNavBarWidth = NavWidth;
-            if(mDisplay != null)
-                setInitialDisplaySize(mDisplay, mUnrestrictedScreenWidth, mUnrestrictedScreenHeight, density);
+            resetScreenHelper();
         }
         if (mHasNavigationBar != showNavBarNow) {
             mHasNavigationBar = showNavBarNow;
-            if(mDisplay != null)
-                setInitialDisplaySize(mDisplay, mUnrestrictedScreenWidth, mUnrestrictedScreenHeight, density);
+            resetScreenHelper();
         }
+        if (mUserUIMode != Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.USER_UI_MODE,mStockUIMode)) {
+            resetScreenHelper();
+        }
+    }
+
+    private void resetScreenHelper() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager wm = (WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(metrics);
+        int density = metrics.densityDpi;
+        if(mDisplay != null)
+            setInitialDisplaySize(mDisplay, mUnrestrictedScreenWidth, mUnrestrictedScreenHeight, density);
     }
 
     private void enablePointerLocation() {
