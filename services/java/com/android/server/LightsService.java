45c0d96b75ccc656956b654f26a08a4e2a090b20
JBirdVegas <jbirdvegas@gmail.com>
2012-12-21 23:52:55 -0500

    Avoid possible memory leaks by ensuring stream closures
    Stream close() methods should always be called in finally block
    to ensure we don't accidentally leak a stream.
    
    Time to fix some old sloppy code.
    
    Change-Id: I3460b558ed5b4489296388c9a52fbb773cefd31f

--- services/java/com/android/server/LightsService.java
+++ services/java/com/android/server/LightsService.java
@@ -142,13 +142,16 @@ public class LightsService {
         private static final String FLASHLIGHT_FILE = "/sys/class/leds/spotlight/brightness";
 
         public boolean getFlashlightEnabled() {
+            FileInputStream fis = null;
             try {
-                FileInputStream fis = new FileInputStream(FLASHLIGHT_FILE);
+                fis = new FileInputStream(FLASHLIGHT_FILE);
                 int result = fis.read();
-                fis.close();
                 return (result != '0');
             } catch (Exception e) {
                 return false;
+            } finally {
+                if (fis != null)
+                    fis.close();
             }
         }
 
@@ -159,15 +162,18 @@ public class LightsService {
                     != PackageManager.PERMISSION_GRANTED) {
                 throw new SecurityException("Requires FLASHLIGHT or HARDWARE_TEST permission");
             }
+            FileOutputStream fos = null;
             try {
-                FileOutputStream fos = new FileOutputStream(FLASHLIGHT_FILE);
+                fos = new FileOutputStream(FLASHLIGHT_FILE);
                 byte[] bytes = new byte[2];
                 bytes[0] = (byte)(on ? '1' : '0');
                 bytes[1] = '\n';
                 fos.write(bytes);
-                fos.close();
             } catch (Exception e) {
                 // fail silently
+            } finally {
+                if (fos != null)
+                    fos.close();
             }
         }
     };
