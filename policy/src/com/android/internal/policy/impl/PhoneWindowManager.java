27bdc95e9352280d0feecafd6352311f0aa203fc
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-01-08 18:58:19 +0100

    Don't wake the screen for media playback KeyEvents
    Media playback KeyEvents wake up the screen, except HEADSETHOOK.
    Add rest of the playback events to the filter.
    
    Filtered KeyEvent list after this patch:
    HEADSETHOOK
    MEDIA_PLAY_PAUSE
    MEDIA_STOP
    MEDIA_NEXT
    MEDIA_PREVIOUS
    MEDIA_REWIND
    MEDIA_FAST_FORWARD
    MEDIA_PLAY
    MEDIA_PAUSE
    
    Change-Id: I378e33081e9c43c44a28bdd29ce221d48b6108c4
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3541,9 +3541,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     && ((keyCode == KeyEvent.KEYCODE_VOLUME_UP) || (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN))) {
             isWakeKey = false;
         }
-
-        // don't wake the screen for headset play/pause
-        if ((keyCode == KeyEvent.KEYCODE_HEADSETHOOK) && isWakeKey) {
+        Log.d(TAG, "received key: " + String.valueOf(keyCode));
+        // don't wake the screen for media events
+        if ((keyCode == KeyEvent.KEYCODE_HEADSETHOOK
+                || ((keyCode >= KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE)
+                && (keyCode <= KeyEvent.KEYCODE_MEDIA_FAST_FORWARD))
+                || (keyCode == KeyEvent.KEYCODE_MEDIA_PLAY)
+                || (keyCode == KeyEvent.KEYCODE_MEDIA_PAUSE))
+                && isWakeKey) {
             isWakeKey = false;
         }
 
