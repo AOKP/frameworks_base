d183a04ee49b0f798612b92c854dc75b9688dd9c
Roman Birg <romanbirg@gmail.com>
2013-01-14 00:55:24 -0600

    SystemUI: cleaner recents key implementation
    Also, I noticed that the RecentsTaskLoader implemented
    View.OnTouchListener, so just use that implementation for the button.
    Also camelCased some variables.
    
    Change-Id: If51c592bebcfbe98d49bb362f845a1b1d86cfb1e
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- packages/SystemUI/src/com/android/systemui/aokp/AokpTarget.java
+++ packages/SystemUI/src/com/android/systemui/aokp/AokpTarget.java
@@ -89,7 +89,6 @@ public class AokpTarget {
 	public final static String ACTION_SEARCH = "**search**";
     public final static String ACTION_NULL = "**null**";
 
-    private boolean mRecentButtonLock = false;
     private int mInjectKeyCode;
     private Context mContext;
     private Handler mHandler;
@@ -103,27 +102,6 @@ public class AokpTarget {
     }
 
     public boolean launchAction (String action){
-
-        if (action.equals(ACTION_RECENTS)) {
-            if (!mRecentButtonLock) {
-                try {
-                    IStatusBarService.Stub.asInterface(
-                            ServiceManager.getService(Context.STATUS_BAR_SERVICE))
-                            .toggleRecentApps();
-                } catch (RemoteException e) {
-                    // nuu
-                }
-                mRecentButtonLock = true;
-                // 250ms animation duration + 150ms start delay of animation + 1 for good luck
-                mHandler.postDelayed(mUnlockRecents, 401);
-            }
-            return true;
-        }
-        try {
-            ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
-        } catch (RemoteException e) {
-        }
-
         if (action == null || action.equals(ACTION_NULL)) {
             return false;
         } else if (action.equals(ACTION_HOME)) {
@@ -410,13 +388,6 @@ public class AokpTarget {
                     am.forceStopPackage(packageName);
                     Toast.makeText(mContext, R.string.app_killed_message, Toast.LENGTH_SHORT).show();
             }
-        }
-    };
-
-    final Runnable mUnlockRecents = new Runnable() {
-        @Override
-        public void run() {
-            mRecentButtonLock = false;
         }
     };
 
