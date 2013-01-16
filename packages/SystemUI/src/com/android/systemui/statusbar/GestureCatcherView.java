4d3438182418b65dd71a6f5da20492d3e097016f
Zaphod <mwilson@acesappraisal.com>
2013-01-16 11:29:19 -0600

    FW: Hiding NavBar - {WIP - DO NOT MERGE}
    PS8:
    New Structure for Hiding NavBar
    
    ps9 : Fix a few derps.
    ps10 : more derps/Try/catch/ etc.
    ps11 : Hiding NavBar now Marginall works.
           There are a lot of things that can break this right now.
           it doesn't recover well from toggling itself on/off and
           if you 'hide/show' the NavBar, it doesn't take to that kindly
           either.  One thing the seems have an effect is to change the
           height of the NavBar.  there is something that gets reset in
           PhoneWindowManager by doing the height change that helps.
    
    ps 12 :  Reworked some of the logic.  Added visual hints to indicate where
    the swipes should occur.  Also completed landscape (phone) orientation shift
    for swipe.  Made several changes in the window layout formulas in
    PhoneWindowManager so that NavBar is treated like a real overlay when
    in AutoHide Mode.  Otherwise, certain windows and elements were
    resizing when the NavBar was shown/hidden.
    
    ps 13:  Add images (button/drag) from NitrozK
    
    ps14:  Start implementation of longpress on handle to launch
           NavRing.  Fix press navbar resets hide timer.
    
    ps 15: add timeout to settings observer
           make searchlight visile on lockscreen even if navbar hide enabled.
    Mike and I think there no point in wasting a swipe on the lockscreen lets
    just show it all the time since its our version of lockscreen targets. Only
    thing thats weird is after unlock the navbar should auto time out and its not.
    It wait until you touch outside then acts as it should.
    
    ps 16: Fix hdpi image
    ps 17: switched to 9patch's from nitroz
           added options for stretching draghandle/swipe area width (on tested on
                  grouper might need tweaking of values)
           added option to make draghandle.invisible (right now if invisible wont
                  register clicks, needs fix)
    
    ps 18: moved constants to demins for overlays
    
    Change-Id: Ic4ad3d9dec139ebeee2ee1e81d97ec603116f5f9

---
+++ packages/SystemUI/src/com/android/systemui/statusbar/GestureCatcherView.java
@@ -0,0 +1,172 @@
+package com.android.systemui.statusbar;
+
+import com.android.systemui.R;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class GestureCatcherView extends LinearLayout{
+
+    private Context mContext;
+    private Resources res;
+    private Handler mHandler;
+    private ImageView mDragButton;
+    long mDowntime;
+    int mTimeOut;
+    private int mWidth;
+    private int mMultiplier;
+    private int mConstant;
+    private boolean mDragButtonVisible;
+
+    private int mTriggerThreshhold = 20;
+    private float[] mDownPoint = new float[2];
+    private boolean mSwapXY = false;
+    private boolean mNavBarSwipeStarted = false;;
+
+    private BaseStatusBar mBar;
+
+    final static String TAG = "PopUpNav";
+
+    public GestureCatcherView(Context context, AttributeSet attrs, BaseStatusBar sb) {
+        super(context, attrs);
+
+        Log.d(TAG,"NavPopupView Constructor");
+        mContext = context;
+        mHandler = new Handler();
+        mBar = sb;
+        res = mContext.getResources();
+        mConstant = res.getDimensionPixelSize(R.dimen.drag_handle_constant);
+        mMultiplier = res.getDimensionPixelSize(R.dimen.drag_handle_multiplier);
+        mDragButton = new ImageView(mContext);
+        mDragButton.setScaleType(ImageView.ScaleType.FIT_XY);
+        mDragButton.setImageDrawable(mContext.getResources().getDrawable(R.drawable.navbar_drag_button));
+        addView(mDragButton);
+
+        SettingsObserver settingsObserver = new SettingsObserver(new Handler());
+        settingsObserver.observe();
+        updateSettings();
+
+        mDragButton.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                Log.d(TAG,"got Gesture Action:");
+                if (action == MotionEvent.ACTION_DOWN) {
+                    if (mNavBarSwipeStarted) {
+                    } else {
+                        Log.d(TAG,"got initial down");
+                        mDownPoint[0] = event.getX();
+                        mDownPoint[1] = event.getY();
+                        mNavBarSwipeStarted = true;
+                    }
+                }
+                if (action == MotionEvent.ACTION_CANCEL) {
+                    mNavBarSwipeStarted = false;
+                }
+                if (action == MotionEvent.ACTION_MOVE && mNavBarSwipeStarted) {
+                    final int historySize = event.getHistorySize();
+                    for (int k = 0; k < historySize + 1; k++) {
+                        float x = k < historySize ? event.getHistoricalX(k) : event.getX();
+                        float y = k < historySize ? event.getHistoricalY(k) : event.getY();
+                        float distance = 0f;
+                        distance = mSwapXY ? (mDownPoint[0] - x) : (mDownPoint[1] - y);
+                        if (distance > mTriggerThreshhold) {
+                            mNavBarSwipeStarted = false;
+                            mBar.showBar(false);
+                        }
+                    }
+                }
+             // fake a touch on the NavRing
+                mBar.mSearchPanelView.dispatchTouchEvent(event);
+                return false;
+            }
+        });
+        mDragButton.setOnLongClickListener(new View.OnLongClickListener() {
+
+            @Override
+            public boolean onLongClick(View v) {
+                mBar.showBar(true);
+                return true;
+            }
+        });
+    }
+
+    public void setWeights() {
+        //just a test
+        LayoutParams DragParams = (LayoutParams) mDragButton.getLayoutParams();
+        DragParams.width = (mWidth * mMultiplier) + mConstant;
+        mDragButton.setLayoutParams(DragParams);
+    }
+
+    public void setSwapXY(boolean swap) {
+        mSwapXY = swap;
+        if (mSwapXY) {
+            mDragButton.setImageDrawable(mContext.getResources().getDrawable(R.drawable.navbar_drag_button_land));
+        } else {
+            mDragButton.setImageDrawable(mContext.getResources().getDrawable(R.drawable.navbar_drag_button));
+        }
+    }
+
+    public WindowManager.LayoutParams getGesturePanelLayoutParams() {
+        boolean onSide = (mContext.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            && (Settings.System.getInt(mContext.getContentResolver(),Settings.System.CURRENT_UI_MODE, 0) == 0);
+        WindowManager.LayoutParams lp  = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                PixelFormat.TRANSLUCENT);
+        lp.gravity = (onSide ? Gravity.CENTER_VERTICAL | Gravity.RIGHT : Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
+        lp.setTitle("GesturePanel");
+        return lp;
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DRAG_HANDLE_WEIGHT), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DRAG_HANDLE_VISIBLE), false, this);
+        }
+         @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+   protected void updateSettings() {
+        ContentResolver cr = mContext.getContentResolver();
+        mDragButtonVisible = Settings.System.getBoolean(cr, Settings.System.DRAG_HANDLE_VISIBLE, true);
+
+        if (!mDragButtonVisible) {
+            mDragButton.setVisibility(View.INVISIBLE);
+            mDragButton.setClickable(true);
+        } else {
+            mDragButton.setVisibility(View.VISIBLE);
+            mDragButton.setClickable(true);
+        }
+
+        mWidth = Settings.System.getInt(cr, Settings.System.DRAG_HANDLE_WEIGHT, 0);
+        setWeights();
+    }
+}
