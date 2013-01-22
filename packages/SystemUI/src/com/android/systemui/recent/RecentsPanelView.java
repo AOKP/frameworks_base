2b7bcc2eb74715f356c7048250fb38f4e9961c50
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-01-22 21:14:18 +0100

    RAMBar - show available memory in the old way
    Old way means handle cached memory like it was free, subtract it from total.
    Also use the internal MemoryInfo class instad of reading from /proc/meminfo.
    
    Change-Id: I96462ed5f1b6923ec8fc4405b225d57691dd1aa0
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>
    
    SystemUI: Recents window visual updates
    
    * Changed the KillAll button to not have a border/be transparent
    * Stretched out the RAM bar across the top, and add a small shadow underneath
    
    PS4: Drop landscape related changes down
         from GNow commit (wrong patch)
    
    Change-Id: Iabdab0a083b00ee51834415f0454f8298efe5591
    
    recents: turn Kill All into normal button
    
    Change-Id: Ie7783449e593ceab81039133fcee2f1561954ef3
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -43,6 +43,7 @@ import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.text.format.Formatter;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -72,9 +73,6 @@ import com.android.systemui.statusbar.phone.PhoneStatusBar;
 
 import com.android.internal.util.MemInfoReader;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class RecentsPanelView extends FrameLayout implements OnItemClickListener, RecentsCallback,
@@ -107,14 +105,12 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     boolean ramBarEnabled;
     boolean mRecentsKillAllEnabled;
 
-    private long mFreeMemory;
-    private long mTotalMemory;
-
     TextView mBackgroundProcessText;
     TextView mForegroundProcessText;
 
     Handler mHandler = new Handler();
     SettingsObserver mSettingsObserver;
+    ActivityManager mAm;
 
     MemInfoReader mMemInfoReader = new MemInfoReader();
 
@@ -293,6 +289,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         super(context, attrs, defStyle);
         updateValuesFromResources();
 
+        mAm = (ActivityManager)
+                mContext.getSystemService(Context.ACTIVITY_SERVICE);
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RecentsPanelView,
                 defStyle, 0);
 
@@ -392,6 +390,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         if (root != null) {
             root.setDrawDuringWindowsAnimating(true);
         }
+        mSettingsObserver.observe(); // observe will call updateSettings()
     }
 
     public void onUiHidden() {
@@ -691,8 +690,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         ViewHolder holder = (ViewHolder) view.getTag();
         TaskDescription ad = holder.taskDescription;
         final Context context = view.getContext();
-        final ActivityManager am = (ActivityManager)
-                context.getSystemService(Context.ACTIVITY_SERVICE);
 
         Bitmap bm = null;
         boolean usingDrawingCache = true;
@@ -715,7 +712,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         show(false);
         if (ad.taskId >= 0) {
             // This is an active task; it should just go to the foreground.
-            am.moveTaskToFront(ad.taskId, ActivityManager.MOVE_TASK_WITH_HOME,
+            mAm.moveTaskToFront(ad.taskId, ActivityManager.MOVE_TASK_WITH_HOME,
                     opts);
         } else {
             Intent intent = ad.intent;
@@ -761,10 +758,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
 
         // Currently, either direction means the same thing, so ignore direction and remove
         // the task.
-        final ActivityManager am = (ActivityManager)
-                mContext.getSystemService(Context.ACTIVITY_SERVICE);
-        if (am != null) {
-            am.removeTask(ad.persistentTaskId, ActivityManager.REMOVE_TASK_KILL_PROCESS);
+        if (mAm != null) {
+            mAm.removeTask(ad.persistentTaskId, ActivityManager.REMOVE_TASK_KILL_PROCESS);
 
             // Accessibility feedback
             setContentDescription(
@@ -849,11 +844,9 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     }
 
     private void killAllRecentApps(){
-        final ActivityManager am = (ActivityManager)
-                mContext.getSystemService(Context.ACTIVITY_SERVICE);
         if(!mRecentTaskDescriptions.isEmpty()){
             for(TaskDescription ad : mRecentTaskDescriptions){
-                am.removeTask(ad.persistentTaskId, ActivityManager.REMOVE_TASK_KILL_PROCESS);
+                mAm.removeTask(ad.persistentTaskId, ActivityManager.REMOVE_TASK_KILL_PROCESS);
                 // Accessibility feedback
                 setContentDescription(
                         mContext.getString(R.string.accessibility_recents_item_dismissed, ad.getLabel()));
@@ -872,14 +865,24 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             if (!ramBarEnabled)
                 return;
 
-            updateMemoryInfo();
+            ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+            mAm.getMemoryInfo(memInfo);
+            long secServerMem = memInfo.secondaryServerThreshold;
+            mMemInfoReader.readMemInfo();
+            long availMem = mMemInfoReader.getFreeSize() + mMemInfoReader.getCachedSize() -
+                    secServerMem;
+            long totalMem = mMemInfoReader.getTotalSize();
+
+            String sizeStr = Formatter.formatShortFileSize(mContext, totalMem-availMem);
             mForegroundProcessText.setText(getResources().getString(
-                    R.string.service_foreground_processes, (mTotalMemory - mFreeMemory) + " mb"));
+                    R.string.service_foreground_processes, sizeStr));
+            sizeStr = Formatter.formatShortFileSize(mContext, availMem);
             mBackgroundProcessText.setText(getResources().getString(
-                    R.string.service_background_processes, mFreeMemory + " mb"));
-            float totalMem = mTotalMemory;
-            float totalShownMem = mFreeMemory;
-            mRamUsageBar.setRatios((totalMem - totalShownMem) / totalMem, 0, 0);
+                    R.string.service_background_processes, sizeStr));
+
+            float fTotalMem = totalMem;
+            float fAvailMem = availMem;
+            mRamUsageBar.setRatios((fTotalMem - fAvailMem) / fTotalMem, 0, 0);
         }
     };
 
@@ -943,43 +946,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             if (mRecentsKillAllButton != null) {
                 mRecentsKillAllButton.setVisibility(View.GONE);
             }
-        }
-    }
-
-    private void updateMemoryInfo() {
-        long result = 0;
-        try {
-            String firstLine = readLine("/proc/meminfo", 1);
-            if (firstLine != null) {
-                String parts[] = firstLine.split("\\s+");
-                if (parts.length == 3) {
-                    result = Long.parseLong(parts[1])/1024;
-                }
-            }
-        } catch (IOException e) {}
-        mTotalMemory = result;
-
-        try {
-            String firstLine = readLine("/proc/meminfo", 2);
-            if (firstLine != null) {
-                String parts[] = firstLine.split("\\s+");
-                if (parts.length == 3) {
-                    result = Long.parseLong(parts[1])/1024;
-                }
-            }
-        } catch (IOException e) {}
-        mFreeMemory = result;
-    }
-
-    private static String readLine(String filename, int line) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
-        try {
-            for(int i = 1; i < line; i++) {
-                reader.readLine();
-            }
-            return reader.readLine();
-        } finally {
-            reader.close();
         }
     }
 }
