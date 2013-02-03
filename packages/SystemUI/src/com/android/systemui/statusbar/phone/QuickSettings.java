8fca6e2a2ce793a81fde189a4c0697870f256565
kecinzer <kecinzer@gmail.com>
2013-02-03 20:06:35 +0100

    FW: Status Bar Hide toggle (1/2)
    Adds new statusbar toggle into power menu navbar control.
    Tested on maguro.
    
    Patch-set 4: Reverted selected button logic
    
    Patch-set 5: Reverted of reverted :)
    
    Patch-set 6: Added czech translation and RC part
    
    Patch-set 7: Added quicksettings tile and shortcut intent
    
    Patch-set 8: Fixed build errors
    
    Patch-set 10: Some czech translations
    
    Patch-set 11: New icons, changed variable names, fixed show logic
    
    Patch-set 12: Rebase
    
    Change-Id: Iafdbac9fb5830762c12ac0d9a6569723f39a64de
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -132,6 +132,7 @@ public class QuickSettings {
     private static final int FAV_CONTACT_TILE = 23;
    // private static final int BT_TETHER_TILE = 23;
     private static final int SOUND_STATE_TILE = 24;
+    private static final int STATUSBAR_TILE = 25;
 
     public static final String USER_TOGGLE = "USER";
     public static final String BRIGHTNESS_TOGGLE = "BRIGHTNESS";
@@ -159,6 +160,7 @@ public class QuickSettings {
     public static final String LTE_TOGGLE = "LTE";
     public static final String FAV_CONTACT_TOGGLE = "FAVCONTACT";
     public static final String SOUND_STATE_TOGGLE = "SOUNDSTATE";
+    public static final String STATUSBAR_TOGGLE = "STATUSBAR";
 
     private static final String DEFAULT_TOGGLES = "default";
 
@@ -239,6 +241,7 @@ public class QuickSettings {
             toggleMap.put(LTE_TOGGLE, LTE_TILE);
             toggleMap.put(FAV_CONTACT_TOGGLE, FAV_CONTACT_TILE);
             toggleMap.put(SOUND_STATE_TOGGLE, SOUND_STATE_TILE);
+            toggleMap.put(STATUSBAR_TOGGLE, STATUSBAR_TILE);
             //toggleMap.put(BT_TETHER_TOGGLE, BT_TETHER_TILE);
         }
         return toggleMap;
@@ -1326,6 +1329,41 @@ public class QuickSettings {
                         iv.setImageDrawable(us.avatar);
                         view.setContentDescription(mContext.getString(
                                 R.string.accessibility_quick_settings_user, state.label));
+                    }
+                });
+                break;
+            case STATUSBAR_TILE:
+                quick = (QuickSettingsTileView)
+                        inflater.inflate(R.layout.quick_settings_tile, parent, false);
+                quick.setContent(R.layout.quick_settings_tile_statusbar, inflater);
+                quick.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    boolean StatusbarState = Settings.System.getBoolean(mContext.getContentResolver(),
+                                 Settings.System.STATUSBAR_HIDDEN, false);
+                        Settings.System.putBoolean(mContext.getContentResolver(),
+                                 Settings.System.STATUSBAR_HIDDEN, !StatusbarState);
+                    }
+                });
+                quick.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        Intent intent = new Intent("android.intent.action.MAIN");
+                        // Try to jump directly into UI settings
+                        intent.setComponent(ComponentName.
+                                unflattenFromString("com.aokp.romcontrol/.ROMControlActivity"));
+                        intent.addCategory("android.intent.category.LAUNCHER");
+                        startSettingsActivity(intent);
+                        return true;
+                    }
+                });
+                mModel.addStatusBarTile(quick, new QuickSettingsModel.RefreshCallback() {
+                    @Override
+                    public void refreshView(QuickSettingsTileView view, State state) {
+                        TextView tv = (TextView) view.findViewById(R.id.statusbar_hide_textview);
+                        tv.setCompoundDrawablesWithIntrinsicBounds(0, state.iconId, 0, 0);
+                        tv.setText(state.label);
+                        tv.setTextSize(1, mTileTextSize);
                     }
                 });
                 break;
