aecea1904dd86ecb54d56196eb1064119f55a37d
Roman Birg <romanbirg@gmail.com>
2013-02-17 02:21:34 -0600

    toggles rewrite WIP
    TODO:
    
        * cleanup code formatting
        * add hooks to tablet classes
        * fix clock spacing on tile
        * identifying which toggles are available
      		-> ROMControl will query sysui to see what's available
    
    Change-Id: I80ef0c3f1cbe8e0d35c7a4e4111ad22b96196963

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3375,6 +3375,11 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_ALPHA_CONFIG = "status_bar_alpha_config";
+
+        /**
+         * @hide
+         */
+        public static final String TOGGLES_STYLE = "toggls_style";
     }
 
     /**
