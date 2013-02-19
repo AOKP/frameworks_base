c342da4943189bc61ed9e2f5b9407f57bef6fd8d
Stevespear426 <stephen.k.spear@gmail.com>
2013-02-20 00:41:16 +0100

    FW: Navring Rework.
    Moves drawables and static strings to Frameworks, Removes
    some redundant code from RC.
    
    Needs awesome action update for now if we dont add it ill
    rebase.
    
    Rebase and clean up, plus I added the option to turn "off" the
    navring by setting all targets to null or none.
    
    added logic for custom icons
    
    forgot to add Icons to the settings observer
    
    adjust some long press logic
    
    Change-Id: Iff157e4e49407f46284f52697befe6cc3d5a42d7

---
+++ core/java/com/android/internal/util/aokp/AwesomeConstants.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2013 AOKP by Mike Wilson - Zaphod-Beeblebrox && Steve Spear - Stevespear426
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.aokp;
+
+public class AwesomeConstants {
+
+    public static final String ASSIST_ICON_METADATA_NAME = "com.android.systemui.action_assist_icon";
+
+    public final static String ACTION_HOME = "**home**";
+    public final static String ACTION_BACK = "**back**";
+    public final static String ACTION_SCREENSHOT = "**screenshot**";
+    public final static String ACTION_MENU = "**menu**";
+    public final static String ACTION_POWER = "**power**";
+    public final static String ACTION_NOTIFICATIONS = "**notifications**";
+    public final static String ACTION_RECENTS = "**recents**";
+    public final static String ACTION_IME = "**ime**";
+    public final static String ACTION_KILL = "**kill**";
+    public final static String ACTION_ASSIST = "**assist**";
+    public final static String ACTION_CUSTOM = "**custom**";
+    public final static String ACTION_SILENT = "**ring_silent**";
+    public final static String ACTION_VIB = "**ring_vib**";
+    public final static String ACTION_SILENT_VIB = "**ring_vib_silent**";
+    public final static String ACTION_EVENT = "**event**";
+    public final static String ACTION_ALARM = "**alarm**";
+    public final static String ACTION_TODAY = "**today**";
+    public final static String ACTION_CLOCKOPTIONS = "**clockoptions**";
+    public final static String ACTION_VOICEASSIST = "**voiceassist**";
+    public final static String ACTION_TORCH = "**torch**";
+    public final static String ACTION_SEARCH = "**search**";
+    public final static String ACTION_LAST_APP = "**lastapp**";
+    public final static String ACTION_RECENTS_GB = "**recentsgb**";
+    public final static String ACTION_NULL = "**null**";
+
+    public final static int INT_ACTION_HOME = 0;
+    public final static int INT_ACTION_BACK = 1;
+    public final static int INT_ACTION_SCREENSHOT = 2;
+    public final static int INT_ACTION_MENU = 3;
+    public final static int INT_ACTION_POWER = 4;
+    public final static int INT_ACTION_NOTIFICATIONS = 5;
+    public final static int INT_ACTION_RECENTS = 6;
+    public final static int INT_ACTION_IME = 7;
+    public final static int INT_ACTION_KILL = 8;
+    public final static int INT_ACTION_ASSIST = 9;
+    public final static int INT_ACTION_CUSTOM = 10;
+    public final static int INT_ACTION_SILENT = 11;
+    public final static int INT_ACTION_VIB = 12;
+    public final static int INT_ACTION_SILENT_VIB = 13;
+    public final static int INT_ACTION_EVENT = 14;
+    public final static int INT_ACTION_ALARM = 15;
+    public final static int INT_ACTION_TODAY = 16;
+    public final static int INT_ACTION_CLOCKOPTIONS = 17;
+    public final static int INT_ACTION_VOICEASSIST = 18;
+    public final static int INT_ACTION_TORCH = 19;
+    public final static int INT_ACTION_SEARCH = 20;
+    public final static int INT_ACTION_LAST_APP = 21;
+    public final static int INT_ACTION_NULL = 22;
+    public final static int INT_ACTION_RECENTS_GB = 23;
+}
