d4094c0eee4fdd9a67ce51dd5b451752490403fc
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-03-08 09:31:40 +0100

    SystemUI: Reboot Toggle
    onClick: 3-way Reboot Menu
    onLongClick: Screen off then reboot
    
    ToDo: Allow invert of onClick<->onLongClick
    
    PS7: Removed dependency on Screenshot toggle
    
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: Ibc601112ede88490c1dd6e5d01122e5037244150

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -89,6 +89,7 @@ public class ToggleManager {
     public static final String QUIETHOURS_TOGGLE = "QUIETHOURS";
     public static final String SLEEP_TOGGLE = "SLEEP";
     public static final String STATUSBAR_TOGGLE = "STATUSBAR";
+    public static final String REBOOT_TOGGLE = "REBOOT";
 
     private int mStyle;
 
@@ -139,6 +140,7 @@ public class ToggleManager {
             toggleMap.put(QUIETHOURS_TOGGLE, QuietHoursToggle.class);
             toggleMap.put(SLEEP_TOGGLE, SleepToggle.class);
             toggleMap.put(STATUSBAR_TOGGLE, StatusbarToggle.class);
+            toggleMap.put(REBOOT_TOGGLE, RebootToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;
@@ -157,6 +159,7 @@ public class ToggleManager {
             }
         };
         mContext.registerReceiver(mBroadcastReceiver, new IntentFilter(ACTION_REQUEST_TOGGLES));
+
     }
 
     public void cleanup() {
