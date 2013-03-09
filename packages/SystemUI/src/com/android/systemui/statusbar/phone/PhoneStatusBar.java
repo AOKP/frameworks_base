b4e2caf2e2f89a9bec692e79abd9e48d66ac318f
Lars Greiss <kufikugel@googlemail.com>
2013-03-09 22:04:01 +0100

    Squash Dark UI commits from Slim
    Frameworks: Add new configuration qualifier for UI change on the fly (1/3)
    
    last night I got an idea after 5 weeks of breaking stuff and
    doing to much complicated things. Together with mnazim we tried
    today to realize this idea. So another absolut new unique SlimBean
    feature is born.
    
    The aim we wanted at the end was to give a simple and resource friendly
    way to change the complete UI of a rom on the fly. So why not use
    the great fallback mechanism of AOSP and change/add a new
    qualifier into the AOSP core. To explain how it works.....if you want
    to add a new UI just add in /res folder of any app or framework you want
    folders with the qualifier "inverted". Eg. drawable-inverted-xhdpi
    or values-inverted or layout-port-inverted or whatever you want. Put in
    only the files you want to modify thats it.
    
    All qualifier in AOSP are following strict rules. You can read here.
    Do not forget the order of qualifier is important. The new "inverted"
    qualifier we added to the UIMode qualifier. To get more information
    about read here
    
    http://developer.android.com/guide/topics/resources/providing-resources.html
    
    So what this commit does:
    
    - add new qualifier "inverted"
    - support for API level 13 and higher
    - change the custom UI on the fly
    - control default UI Mode over config.xml
    - working for all apps. They just need to include folders
      with the new qualifier name
    - resource friendly....no resource wasting either programmticaly
      then source blow, because you just need to put in the files you
      want to change
    - cause of the qualifier structure compatible with every phone,
      language or whatever qualifier you want to use on top
    - 100% compatible to theme manager. If a theme misses drawables
      they are replaced with them from the current UIMode which is set.
    
    What is the benefit why not use just a theme? We started half year
    ago TRDS which was inverted gapps system base themed....means we
    morphed directly the changes into the framework. This methoed was
    on of the reasons why trds got so popular. Points for that are easy
    first the system is themed, means each theme you activate over
    theme engine works on top the themed system. So you have a base
    style and can modify more if you want. The other reason is easy
    a lot faster and resource friendly. Now this idea went to the
    rom to provide two core system based themes and user
    can do on top what they want or not without, like you have
    to roms in one.
    
    This commit just adds the support for this
    
    cheers
    
    mnazim and kufikugel
    
    Change-Id: I1ef843a42d17246373eb17b4e1ec1aa9bc20d8d4
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	services/java/com/android/server/UiModeManagerService.java
    
    Frameworks: TRDS goes Slim
    
    add trds changed files to provide new UI mode engine
    
    Change-Id: I67f8a26320c9971258eb0106c0574b5af9b4261c
    
    Frameworks: TRDS new qualifier push up qualifier number (1/2)
    
    Hey guys,
    
    after this feature is working really well and a lot roms kanged
    it we got a hint to change one thing which is indeed a good
    idea to have less problems on AOSP updates and tools like
    Apktool can provide us better without doing major changes every
    AOSP update.
    
    We pushed the number up to 69.....should be enough for
    next years.
    
    Please note for the one who are using our gapps at the moment
    this will break actuall gapps till we published the new one
    which will be end this week as usual.
    
    After that we will begin to publish the apktool after we had
    now enough tests....and who knows maybe the "official" Apktool
    will provide us all now. So all in all a good time to change
    the qualifier now before it is getting more and more
    popular (we saw that first themes are providing us)
    
    cheers
    
    Lars
    
    Change-Id: Ie1aeab38420cb825f6f7c1d0a5220a8bb55fc4c3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -214,6 +214,7 @@ public class PhoneStatusBar extends BaseStatusBar {
     // settings
     ToggleManager mToggleManager;
     boolean mHasSettingsPanel, mHasFlipSettings;
+    boolean mUiModeIsToggled;
     int mToggleStyle;
     SettingsPanelView mSettingsPanel;
     View mFlipSettingsView;
@@ -480,6 +481,9 @@ public class PhoneStatusBar extends BaseStatusBar {
         mClearButton.setVisibility(View.INVISIBLE);
         mClearButton.setEnabled(false);
         mDateView = (DateView)mStatusBarWindow.findViewById(R.id.date);
+
+        mUiModeIsToggled = Settings.Secure.getInt(mContext.getContentResolver(),
+                              Settings.Secure.UI_MODE_IS_TOGGLED, 0) == 1;
 
         mHasSettingsPanel = res.getBoolean(R.bool.config_hasSettingsPanel);
         mHasFlipSettings = res.getBoolean(R.bool.config_hasFlipSettingsPanel);
@@ -2832,6 +2836,8 @@ public class PhoneStatusBar extends BaseStatusBar {
 
          @Override
         public void onChange(boolean selfChange) {
+            boolean uiModeIsToggled = Settings.Secure.getInt(mContext.getContentResolver(),
+                                    Settings.Secure.UI_MODE_IS_TOGGLED, 0) == 1;
             updateSettings();
         }
     }
