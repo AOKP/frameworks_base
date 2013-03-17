59456091d4f3a6215f9862240342f2fb0d2a7bcd
Bajee11 <bajee11@gmail.com>
2013-03-17 02:05:55 -0400

    Dark AOKP Toggle
    Icon credits: @kroz
    
    Conflicts:
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/res/values/strings_toggles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
    
    Change-Id: I8d4e0e19a0a401c6018392457fda8a48271cf192

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -98,6 +98,7 @@ public class ToggleManager {
     public static final String REBOOT_TOGGLE = "REBOOT";
     public static final String CUSTOM_TOGGLE = "CUSTOM";
     public static final String STAYAWAKE_TOGGLE = "STAYAWAKE";
+    public static final String DARK_TOGGLE = "DARK";
 
     private int mStyle;
 
@@ -166,6 +167,7 @@ public class ToggleManager {
             toggleMap.put(REBOOT_TOGGLE, RebootToggle.class);
             toggleMap.put(CUSTOM_TOGGLE, CustomToggle.class);
             toggleMap.put(STAYAWAKE_TOGGLE, StayAwakeToggle.class);
+            toggleMap.put(DARK_TOGGLE, DarkToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;
