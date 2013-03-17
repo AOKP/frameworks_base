2a99aa2a9b86a4a7de107a734796a2c4aa3ea796
Zaphod <mwilson@acesappraisal.com>
2013-03-17 18:30:35 -0400

    FW : Allow user selection of Font Size
    ps 1:  This re-structures a lot of the way icons & layouts are sized.
    It allows System Icons, Notification Icons, etc. to scale according
    to the size of the statusbar.  StatusBar now sizes itself based on
    the height of the Font (which is user selectable in RC).
    
    This needs a lot of testing, as I've made substantial changes to the
    way the Statusbar.  Also, While everything is 'working', the alignment
    of the Textviews alludes me.  All textviews are being aligned at the bottom
    of the StatusBar rather than centering like they should.
    
    PS 2:  Fix the issue with StatusBar robbing touches at boot.
    Fixed windowing size update in PhoneWindowManager.
    Some of the icons still don't scale exactly right,
    and of course the TextViews continue to taunt me.
    
    ps 3: Re write some of the scaling of images and add some padding.
       Create a StatusBarHelpers sub in core.util.aokp for re-using some routines.
    
    ps 4:  Solved initial sizing issue (at boot or change) and everything
    	seems to be aligned properly.
    	Note that icons will only scale up to their source size.  So
    	themes that provide HQ/large icons can benefit from larger
    	font sizes, but some themes (including stock/system) will
    	show increases in fonts and Notification Icons, but not
    	the SystemIcons (right side)
    
    ps 5: Adjust layout to elimination notification cutoff
    
    ps 6:  Really fix layout this time.
    ps 7: add some padding between notification icons
    
    Change-Id: I5f9e99930210d3d91cda3ef167ad42993c261816
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -943,7 +943,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         final StatusBarIconView iconView = new StatusBarIconView(mContext,
                 notification.pkg + "/0x" + Integer.toHexString(notification.id),
                 notification.notification);
-        iconView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        iconView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        iconView.setAdjustViewBounds(true);
 
         final StatusBarIcon ic = new StatusBarIcon(notification.pkg,
                     notification.user,
