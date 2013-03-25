67802d145ddcf6c1c0b6c1da19b3032d8d55a005
Stevespear426 <stephen.k.spear@gmail.com>
2013-03-25 21:42:37 +0100

    FW: AOKP Ribbon
    Added option to navbar to trigger the bottom ribbon,
    also added long press the navbarautohide handle to trigger the
    bottom ribbon as well.
    
    fixed the layout for the vertical ribbons everything aligned now
    
    added quick option for navbar widgets... peeps been asking
    
    renamed the navbar ribbon to swipe ribbon
    fixed padding when navbar is on
    added logic for background opacity and color
       color isn't working, dont know why same logic as
       navbar but nothing..
    added click listeners for text too
    
    Change-Id: I6bfb541801bd428b5eee06ef1c3f7d23ef7eeede

---
+++ packages/SystemUI/src/com/android/systemui/aokp/AokpSwipeRibbon.java
@@ -0,0 +1,310 @@
+/*
+ * Copyright (C) 2013 The Android Open Kand Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.aokp;
+
+import com.android.systemui.R;
+
+import java.lang.IllegalArgumentException;
+import java.io.File;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+
+import com.android.internal.util.aokp.AokpRibbonHelper;
+import com.android.internal.util.aokp.BackgroundAlphaColorDrawable;
+import com.android.systemui.aokp.RibbonGestureCatcherView;
+
+public class AokpSwipeRibbon extends LinearLayout {
+    public static final String TAG = "NAVIGATION BAR RIBBON";
+
+    private Context mContext;
+    private RibbonGestureCatcherView mGesturePanel;
+    public FrameLayout mPopupView;
+    public WindowManager mWindowManager;
+    private SettingsObserver mSettingsObserver;
+    private LinearLayout mRibbon;
+    private LinearLayout mRibbonMain;
+    private boolean mText, hasNavBarByDefault, NavBarEnabled, navAutoHide;
+    private int mHideTimeOut = 5000;
+    private boolean showing = false;
+    private int mSize, mColor, mOpacity;
+    private String shortTargets, longTargets, mLocation;
+    private Handler mHandler;
+    private boolean[] mEnableSides = new boolean[3];
+
+    public AokpSwipeRibbon(Context context, AttributeSet attrs, String location) {
+        super(context, attrs);
+        mContext = context;
+        mLocation = location;
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(RibbonReceiver.ACTION_TOGGLE_RIBBON);
+        filter.addAction(RibbonReceiver.ACTION_SHOW_RIBBON);
+        filter.addAction(RibbonReceiver.ACTION_HIDE_RIBBON);
+        mContext.registerReceiver(new RibbonReceiver(), filter);
+        mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        mHandler = new Handler();
+        mSettingsObserver = new SettingsObserver(new Handler());
+        mSettingsObserver.observe();
+        updateSettings();
+    }
+
+
+    public void toggleRibbonView() {
+        if (showing) {
+            hideRibbonView();
+            showing = false;
+
+        } else {
+            showRibbonView();
+            showing = true;
+        }
+    }
+
+    public void showRibbonView() {
+        WindowManager.LayoutParams params = getParams();
+        params.gravity = getGravity();
+        params.setTitle("Ribbon" + mLocation);
+        if (mWindowManager != null && !showing){
+            showing = true;
+            mWindowManager.addView(mPopupView, params);
+            if (mGesturePanel != null) {
+                try {
+                    mWindowManager.removeView(mGesturePanel);
+                } catch (IllegalArgumentException e) {
+                    //If we try to remove the gesture panel and it's not currently attached.
+                }
+            }
+            if (mHideTimeOut > 0) {
+                mHandler.postDelayed(delayHide, mHideTimeOut);
+            }
+        }
+    }
+
+    public void hideRibbonView() {
+        if (mPopupView != null) {
+            showing = false;
+            mWindowManager.removeView(mPopupView);
+            if (mGesturePanel != null) {
+                mWindowManager.addView(mGesturePanel, mGesturePanel.getGesturePanelLayoutParams());
+            }
+        }
+    }
+
+    private Runnable delayHide = new Runnable() {
+        public void run() {
+            hideRibbonView();
+        }
+    };
+
+    private WindowManager.LayoutParams getParams() {
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                mLocation.equals("bottom") ? WindowManager.LayoutParams.MATCH_PARENT
+                    : WindowManager.LayoutParams.WRAP_CONTENT,
+                mLocation.equals("bottom") ? WindowManager.LayoutParams.WRAP_CONTENT
+                    : WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                PixelFormat.TRANSLUCENT);
+        return params;
+    }
+
+    private int getGravity() {
+        int gravity = 0;
+        if (mLocation.equals("bottom")) {
+            gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+        } else if (mLocation.equals("left")) {
+            gravity = Gravity.CENTER_VERTICAL | Gravity.LEFT;
+        } else {
+            gravity = Gravity.CENTER_VERTICAL | Gravity.RIGHT;
+        }
+        return gravity;
+    }
+
+    public void createRibbonView() {
+        if (mGesturePanel != null) {
+            try {
+                mWindowManager.removeView(mGesturePanel);
+            } catch (IllegalArgumentException e) {
+                //If we try to remove the gesture panel and it's not currently attached.
+            }
+        }
+        if (sideEnabled()) {
+            mGesturePanel = new RibbonGestureCatcherView(mContext,null,mLocation);
+            mWindowManager.addView(mGesturePanel, mGesturePanel.getGesturePanelLayoutParams());
+        }
+        mPopupView = new FrameLayout(mContext);
+        BackgroundAlphaColorDrawable bacd = new BackgroundAlphaColorDrawable(
+            mColor > 0 ? mColor : Color.BLACK);
+        float opacity = (255f * (mOpacity * 0.01f));
+        bacd.setAlpha((int)opacity);
+        View ribbonView = View.inflate(mContext, R.layout.aokp_swipe_ribbon, null);
+        mRibbon = (LinearLayout) ribbonView.findViewById(R.id.ribbon);
+        mRibbonMain = (LinearLayout) ribbonView.findViewById(R.id.ribbon_main);
+        mRibbonMain.setBackground(bacd);
+        setupRibbon();
+        mPopupView.removeAllViews();
+        mPopupView.addView(ribbonView);
+        mPopupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                    mHandler.removeCallbacks(delayHide);
+                    hideRibbonView();
+                    return true;
+                }
+                mHandler.removeCallbacks(delayHide);
+                mHandler.postDelayed(delayHide, mHideTimeOut);
+                return false;
+            }
+        });
+    }
+
+    private boolean sideEnabled() {
+        if (mLocation.equals("bottom") && mEnableSides[0]) {
+            return true;
+        } else if (mLocation.equals("left") && mEnableSides[1]) {
+            return true;
+        } else if (mLocation.equals("right") && mEnableSides[2]) {
+            return true;
+        }
+        return false;
+    }
+
+    private void setupRibbon() {
+        mRibbon.removeAllViews();
+        if (mLocation.equals("bottom")) {
+            mRibbon.addView(AokpRibbonHelper.getRibbon(mContext,
+                shortTargets, longTargets, mText, mSize));
+        } else {
+            mRibbon.addView(AokpRibbonHelper.getVerticalRibbon(mContext,
+                shortTargets, longTargets, mText, mSize));
+            if (!navAutoHide && (NavBarEnabled || hasNavBarByDefault)) {
+                int adjustment = Settings.System.getInt(
+                    mContext.getContentResolver(),
+                        Settings.System.NAVIGATION_BAR_HEIGHT,
+                    mContext.getResources()
+                        .getDimensionPixelSize(
+                        com.android.internal.R.dimen.navigation_bar_height));
+                mRibbon.setPadding(1,adjustment,1,1);
+            } else {
+                mRibbon.setPadding(1,1,1,1);
+            }
+        }
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RIBBON_TARGETS_SHORT[AokpRibbonHelper.SWIPE_RIBBON]), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RIBBON_TARGETS_LONG[AokpRibbonHelper.SWIPE_RIBBON]), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ENABLE_RIBBON_TEXT[AokpRibbonHelper.SWIPE_RIBBON]), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RIBBON_ICON_SIZE[AokpRibbonHelper.SWIPE_RIBBON]), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RIBBON_HIDE_TIMEOUT), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NAV_HIDE_ENABLE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SWIPE_RIBBON_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SWIPE_RIBBON_OPACITY), false, this);
+            for (int i = 0; i < 3; i++) {
+	            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.ENABLE_RIBBON_LOCATION[i]), false, this);
+            }
+
+        }
+         @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+   protected void updateSettings() {
+        ContentResolver cr = mContext.getContentResolver();
+        shortTargets = Settings.System.getString(cr,
+                 Settings.System.RIBBON_TARGETS_SHORT[AokpRibbonHelper.SWIPE_RIBBON]);
+        longTargets = Settings.System.getString(cr,
+                 Settings.System.RIBBON_TARGETS_LONG[AokpRibbonHelper.SWIPE_RIBBON]);
+        mText = Settings.System.getBoolean(cr,
+                 Settings.System.ENABLE_RIBBON_TEXT[AokpRibbonHelper.SWIPE_RIBBON], true);
+        mSize = Settings.System.getInt(cr,
+                 Settings.System.RIBBON_ICON_SIZE[AokpRibbonHelper.SWIPE_RIBBON], 0);
+        mHideTimeOut = Settings.System.getInt(cr,
+                 Settings.System.RIBBON_HIDE_TIMEOUT, mHideTimeOut);
+        mColor = Settings.System.getInt(cr,
+                 Settings.System.SWIPE_RIBBON_COLOR, 0);
+        mOpacity = Settings.System.getInt(cr,
+                 Settings.System.SWIPE_RIBBON_OPACITY, 255);
+
+        for (int i = 0; i < 3; i++) {
+            mEnableSides[i] = Settings.System.getBoolean(cr,
+                 Settings.System.ENABLE_RIBBON_LOCATION[i], false);
+        }
+        navAutoHide = Settings.System.getBoolean(cr, Settings.System.NAV_HIDE_ENABLE, false);
+        NavBarEnabled = Settings.System.getBoolean(cr, Settings.System.NAVIGATION_BAR_SHOW, false);
+        hasNavBarByDefault = mContext.getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+        mEnableSides[0] = mEnableSides[0] && !navAutoHide && !(NavBarEnabled || hasNavBarByDefault);
+        createRibbonView();
+    }
+
+    public class RibbonReceiver extends BroadcastReceiver {
+        public static final String ACTION_TOGGLE_RIBBON = "com.android.systemui.ACTION_TOGGLE_RIBBON";
+        public static final String ACTION_SHOW_RIBBON = "com.android.systemui.ACTION_SHOW_RIBBON";
+        public static final String ACTION_HIDE_RIBBON = "com.android.systemui.ACTION_HIDE_RIBBON";
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            String location = intent.getStringExtra("action");
+            if (ACTION_TOGGLE_RIBBON.equals(action)) {
+                if (location.equals(mLocation)) {
+                    toggleRibbonView();
+                }
+            } else if (ACTION_SHOW_RIBBON.equals(action)) {
+                if (location.equals(mLocation)) {
+                    showRibbonView();
+                }
+            }
+        }
+    }
+}
