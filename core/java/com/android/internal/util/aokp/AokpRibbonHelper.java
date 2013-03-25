beed5d45c23bdbc835a28a48563613671c4c63f3
Stevespear426 <stephen.k.spear@gmail.com>
2013-03-25 21:42:37 +0100

    FW: AOKP Ribbon
    Added option to navbar to trigger the bottom ribbon,
    also added long press the navbarautohide handle to trigger the
    bottom ribbon as well.
    
    fixed the layout for the vertical ribbons everything aligned now
    
    added quick option for navbar widgets... peeps been asking
    
    renamed the navbar ribbon to swipe ribbon
    fixed padding when navbar is on
    added logic for background opacity and color
       color isn't working, dont know why same logic as
       navbar but nothing..
    added click listeners for text too
    
    Added in and out animations.
    
    Annimations for the navbar widgets too... for Khas
    
    Change-Id: I6bfb541801bd428b5eee06ef1c3f7d23ef7eeede

---
+++ core/java/com/android/internal/util/aokp/AokpRibbonHelper.java
@@ -0,0 +1,82 @@
+package com.android.internal.util.aokp;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+
+public class AokpRibbonHelper {
+
+    private static final String TAG = "Aokp Ribbon";
+
+    private static final String TARGET_DELIMITER = "|";
+    public static final int LOCKSCREEN = 0;
+    public static final int NOTIFICATIONS = 1;
+    public static final int SWIPE_RIBBON = 2;
+
+    private static final LinearLayout.LayoutParams PARAMS_TARGET = new LinearLayout.LayoutParams(
+            LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1f);
+
+    private static final LinearLayout.LayoutParams PARAMS_TARGET_VERTICAL = new LinearLayout.LayoutParams(
+            LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT, 1f);
+
+    private static final LinearLayout.LayoutParams PARAMS_TARGET_SCROLL = new LinearLayout.LayoutParams(
+            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f);
+
+    public static HorizontalScrollView getRibbon(Context mContext, String shortTargets, String longTargets, boolean text, int size) {
+        HorizontalScrollView targetScrollView = new HorizontalScrollView(mContext);
+        if (!TextUtils.isEmpty(shortTargets) && !TextUtils.isEmpty(longTargets)) {
+            final String[] shortSplit = shortTargets.split("\\" + TARGET_DELIMITER);
+            final String[] longSplit = longTargets.split("\\" + TARGET_DELIMITER);
+            ArrayList<RibbonTarget> targets = new ArrayList<RibbonTarget>();
+
+            for (int i = 0; i < shortSplit.length; i++) {
+                RibbonTarget newTarget = null;
+                newTarget = new RibbonTarget(mContext, shortSplit[i], longSplit[i], text, size);
+                if (newTarget != null) {
+                    targets.add(newTarget);
+                }
+            }
+            LinearLayout targetsLayout = new LinearLayout(mContext);
+            targetsLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+            targetScrollView.setHorizontalFadingEdgeEnabled(true);
+            for (int i = 0; i < targets.size(); i++) {
+                targetsLayout.addView(targets.get(i).getView(), PARAMS_TARGET_SCROLL);
+            }
+            targetScrollView.addView(targetsLayout, PARAMS_TARGET);
+        }
+        return targetScrollView;
+    }
+
+    public static ScrollView getVerticalRibbon(Context mContext, String shortTargets, String longTargets, boolean text, int size) {
+        ScrollView targetScrollView = new ScrollView(mContext);
+        if (!TextUtils.isEmpty(shortTargets) && !TextUtils.isEmpty(longTargets)) {
+            final String[] shortSplit = shortTargets.split("\\" + TARGET_DELIMITER);
+            final String[] longSplit = longTargets.split("\\" + TARGET_DELIMITER);
+            ArrayList<RibbonTarget> targets = new ArrayList<RibbonTarget>();
+
+            for (int i = 0; i < shortSplit.length; i++) {
+                RibbonTarget newTarget = null;
+                newTarget = new RibbonTarget(mContext, shortSplit[i], longSplit[i], text, size);
+                if (newTarget != null) {
+                    targets.add(newTarget);
+                }
+            }
+            LinearLayout targetsLayout = new LinearLayout(mContext);
+            targetsLayout.setOrientation(LinearLayout.VERTICAL);
+            targetsLayout.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+            targetScrollView.setOverScrollMode(View.OVER_SCROLL_IF_CONTENT_SCROLLS);
+            for (int i = 0; i < targets.size(); i++) {
+                targetsLayout.addView(targets.get(i).getView(), PARAMS_TARGET_SCROLL);
+            }
+            targetScrollView.addView(targetsLayout, PARAMS_TARGET_VERTICAL);
+        }
+        return targetScrollView;
+    }
+}
