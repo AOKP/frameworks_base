bb123145414260011e19a9250cade47187cc46a7
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-04-14 08:36:36 +0200

    Battery: Show green when battery is 100% and introduce 4th stage
    * Only show full green when battery is 100% instead of >= 90
    * Introduce Yellow increment which includes 70-99%
    	 0-14: red
    	15-69: orange
    	70-99: yellow
              100: green
    
    Change-Id: I24af9bb012744ea15d411c2aa42afe9388f2a97e
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -677,6 +677,7 @@ public final class BatteryService extends Binder {
 
         private final int mBatteryLowARGB;
         private final int mBatteryMediumARGB;
+        private final int mBatteryAlmostARGB;
         private final int mBatteryFullARGB;
         private final int mBatteryLedOn;
         private final int mBatteryLedOff;
@@ -688,6 +689,8 @@ public final class BatteryService extends Binder {
                     com.android.internal.R.integer.config_notificationsBatteryLowARGB);
             mBatteryMediumARGB = context.getResources().getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryMediumARGB);
+            mBatteryAlmostARGB = context.getResources().getInteger(
+                    com.android.internal.R.integer.config_notificationsBatteryAlmostARGB);
             mBatteryFullARGB = context.getResources().getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryFullARGB);
             mBatteryLedOn = context.getResources().getInteger(
@@ -713,11 +716,14 @@ public final class BatteryService extends Binder {
                 }
             } else if (status == BatteryManager.BATTERY_STATUS_CHARGING
                     || status == BatteryManager.BATTERY_STATUS_FULL) {
-                if (status == BatteryManager.BATTERY_STATUS_FULL || level >= 90) {
-                    // Solid green when full or charging and nearly full
+                if (level >= 70 && level < 100) {
+                    // Solid orange when charging and between 70-99%
+                    mBatteryLight.setColor(mBatteryAlmostARGB);
+                } else if (status == BatteryManager.BATTERY_STATUS_FULL || level == 100) {
+                    // Solid green when full -- 100%
                     mBatteryLight.setColor(mBatteryFullARGB);
                 } else {
-                    // Solid orange when charging and halfway full
+                    // Solid yellow when charging and and between 15-69%
                     mBatteryLight.setColor(mBatteryMediumARGB);
                 }
             } else {
