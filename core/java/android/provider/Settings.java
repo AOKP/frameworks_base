8fe6761c167206eb728cffc5d6f4da05700c1cb3
Jubakuba <Jubakuba@gmail.com>
2013-04-23 23:35:44 -0600

    Framework: CustomToggle Improvements (!Advanced)
    Remove advanced Preference in favor of:
            If click = null, use longclick icon.
            This is all Advanced vs Basic was, really.
            User confusion might ensue, but ultimately,
            Exact same ability if you set click to null.
            This also clarifies the ShouldCollapse
            Preference through correlation of it not
            Collapsing on-click in basic.
            Also refresh Icons ondemand.
    
    Change-Id: Ia56db7778c89bb38c5e77719e9fa47a9eef4c7eb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3354,11 +3354,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String CUSTOM_TOGGLE_ADVANCED = "custom_toggle_advanced";
-
-        /**
-         * @hide
-         */
         public static final String CUSTOM_TOGGLE_REVERT = "custom_toggle_revert";
 
         /**
