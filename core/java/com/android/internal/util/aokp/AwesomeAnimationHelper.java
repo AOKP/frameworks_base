add7da9960ca22305d8f8aed2bd5c59e5a40170f
Stevespear426 <stephen.k.spear@gmail.com>
2013-05-09 17:37:09 +0200

    FW: Ribbon v1.0 overhaul
    Thickness Increase max && decrease min
    Configurable long swipe and long press swipe areas
    
    consolidate and cache views for smoother animations
    User adjustable animantion duration
    
    Add dismiss options for swipe ribbon
     - Always dismiss on click
     - never dismiss on click
     - Don't dismiss for navbar action
    
    fix app window crash if user has fast fingers and to much time
      on their hands...
    
    Swipe ribbon drag handles now get their own location, height,
       width, and opacity.
    
    Hide apps from app window
    Hooks to dismiss app window if softkeys or navbar buttons pressed
    
    Fix app window to pull all components also IE camera and messenger.
    
    Rub some unicorn polish on the on touch glow. The giant boxes were
    a bit ridiculous.
    
    If ribbon is empty but toogles are not, just show toggles no flipper.
    
    add adb wireless to toggle map
    
    Phase 1 of the Animation chooser. Simple helpers to let users
    choose the animation for the swipe ribbon and app window.
    ill be adding some fancier animations for now just some of
    the stock AOSP one.
    
    Change-Id: I7c59fc915473fe320639eda446a735dee8500efa

---
+++ core/java/com/android/internal/util/aokp/AwesomeAnimationHelper.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2013 AOKP by Steve Spear - Stevespear426
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.aokp;
+
+import android.content.Context;
+import android.content.res.Resources;
+//import android.view.animation.Animation;
+//import android.view.animation.AnimationUtils;
+
+public class AwesomeAnimationHelper {
+
+    public final static int ANIMATION_DEFAULT = 0;
+    public final static int ANIMATION_FADE = 1;
+    public final static int ANIMATION_SLIDE_RIGHT = 2;
+    public final static int ANIMATION_SLIDE_LEFT = 3;
+    public final static int ANIMATION_SLIDE_UP = 4;
+
+    public static int[] getAnimationsList() {
+        int[] anim = new int[5];
+        anim[0] = ANIMATION_DEFAULT;
+        anim[1] = ANIMATION_FADE;
+        anim[2] = ANIMATION_SLIDE_RIGHT;
+        anim[3] = ANIMATION_SLIDE_LEFT;
+        anim[4] = ANIMATION_SLIDE_UP;
+        return anim;
+    }
+
+    public static int[] getAnimations(int mAnim) {
+        int[] anim = new int[2];
+        switch (mAnim) {
+            case ANIMATION_FADE:
+                anim[0] = com.android.internal.R.anim.fade_out;
+                anim[1] = com.android.internal.R.anim.fade_in;
+                break;
+            case ANIMATION_SLIDE_RIGHT:
+                anim[0] = com.android.internal.R.anim.slide_out_right;
+                anim[1] = com.android.internal.R.anim.slide_in_right;
+                break;
+            case ANIMATION_SLIDE_LEFT:
+                anim[0] = com.android.internal.R.anim.slide_out_left;
+                anim[1] = com.android.internal.R.anim.slide_in_left;
+                break;
+            case ANIMATION_SLIDE_UP:
+                anim[0] = com.android.internal.R.anim.slide_out_down;
+                anim[1] = com.android.internal.R.anim.slide_in_up;
+                break;
+        }
+        return anim;
+    }
+
+    public static String getProperName(Context context, int mAnim) {
+        Resources res = context.getResources();
+        String value = "";
+        switch (mAnim) {
+            case ANIMATION_DEFAULT:
+                value = res.getString(com.android.internal.R.string.animation_default);
+                break;
+            case ANIMATION_FADE:
+                value = res.getString(com.android.internal.R.string.animation_fade);
+                break;
+            case ANIMATION_SLIDE_RIGHT:
+                value = res.getString(com.android.internal.R.string.animation_slide_right);
+                break;
+            case ANIMATION_SLIDE_LEFT:
+                value = res.getString(com.android.internal.R.string.animation_slide_left);
+                break;
+            case ANIMATION_SLIDE_UP:
+                value = res.getString(com.android.internal.R.string.animation_slide_up);
+                break;
+            default:
+                value = res.getString(com.android.internal.R.string.action_null);
+                break;
+
+        }
+        return value;
+    }
+}
