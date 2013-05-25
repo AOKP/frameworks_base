af8207c2bc2b5654b61b72329973b5c7769cb4ae
Zaphod <mwilson@acesappraisal.com>
2013-05-25 17:46:14 -0400

    FW:  Bring Autohide to SystemBar (1/2)
    AutoHide for SystemBar (TabUI)
    
    Change-Id: I194d4e0d0343aac809bcee8b93a0ddd43341c64a
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1191,7 +1191,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case 1 :
                 // "tablet" UI with a single combined status & navigation bar
-                mNavBarAutoHide = false; // TabUI, No AutoHide for you!
+                //mNavBarAutoHide = false; // TabUI, No AutoHide for you!
                 mHasSystemNavBar = true;
                 mNavigationBarCanMove = false;
                 break;
@@ -1736,7 +1736,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     public int getNonDecorDisplayHeight(int fullWidth, int fullHeight, int rotation) {
-        if (mHasSystemNavBar) {
+        if (mHasSystemNavBar && !mNavBarAutoHide) {
             // For the system navigation bar, we always place it at the bottom.
             return fullHeight - mNavigationBarHeightForRotation[rotation];
         }
