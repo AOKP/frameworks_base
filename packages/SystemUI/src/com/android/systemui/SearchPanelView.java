b219998b4cfc7fe67bc7ab11cd9d21061bf62da2
Jubakuba <Jubakuba@gmail.com>
2013-06-12 05:08:57 -0600

    NavRing: Longpress Logic
    Again, this matches shortpress logic.
        (And now matches longpress logic
        since this was merged on Lockscreen)
    
    Vibrates on Longpress...
        Remove finger to launch.
    
    Change-Id: Ia7f8bb2580643930511fd12764f4a41873ef64d2

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -232,15 +232,10 @@ public class SearchPanelView extends FrameLayout implements
 
        final Runnable SetLongPress = new Runnable () {
             public void run() {
-                if (!mSearchPanelLock) {
-                    mSearchPanelLock = true;
+                if (!mLongPress) {
+                    vibrate();
                     mLongPress = true;
-                    if (shouldUnlock(longList.get(mTarget))) {
-                        maybeSkipKeyguard();
-                    }
-                    AwesomeAction.launchAction(mContext, longList.get(mTarget));
-                    mBar.hideSearchPanel();
-                 }
+                }
             }
         };
 
@@ -262,6 +257,14 @@ public class SearchPanelView extends FrameLayout implements
 
         public void onReleased(View v, int handle) {
             fireTorch();
+            if (!mSearchPanelLock && mLongPress) {
+                mSearchPanelLock = true;
+                if (shouldUnlock(longList.get(mTarget))) {
+                    maybeSkipKeyguard();
+                }
+                AwesomeAction.launchAction(mContext, longList.get(mTarget));
+                mBar.hideSearchPanel();
+            }
         }
 
         public void onTargetChange(View v, final int target) {
