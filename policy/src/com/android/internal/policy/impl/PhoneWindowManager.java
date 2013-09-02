0867a3a18ba9148f31274f99905bf6c86d32b942
PrimeDirective <activethrasher00@gmail.com>
2013-09-01 23:25:27 -0400

    fix navbar transparency
    Change-Id: I6c302e86ad5657a9dfb5072a2e70bdc8cee4ec83

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2973,7 +2973,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     if (navVisible) {
                         mNavigationBar.showLw(true);
                         if (!mNavBarAutoHide)
-                            mSystemBottom = mDockBottom = mTmpNavigationFrame.bottom - mDockTop;
+                            mSystemBottom = mDockBottom = mTmpNavigationFrame.top - mDockTop;
+                            mRestrictedOverscanScreenHeight = mDockBottom - mRestrictedOverscanScreenTop;
                     } else {
                         // We currently want to hide the navigation UI.
                         mNavigationBar.hideLw(true);
