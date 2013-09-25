ee15f100f072bc4a8ec008ceab490f12d937c294
LorDClockaN <davor@losinj.com>
2013-09-25 23:39:58 +0200

    FW: Option to hide Camera widget from lockscreen (1/2)
    PS2: Update SettingsString Typo
    
    Change-Id: I531e16ffe2c789b8d9987ca89673d96fd8927f32

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3813,6 +3813,14 @@ public final class Settings {
         public static final String THEME_WALLPAPER = "theme_wallpaper";
 
         /**
+         * Allows to disable Camera widget in locskcreen
+         * 0 = hide camera
+         * 1 = show camera - default
+         * @hide
+         */
+        public static final String LOCKSCREEN_CAMERA_WIDGET_SHOW = "lockscreen_camera_widget_show";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
