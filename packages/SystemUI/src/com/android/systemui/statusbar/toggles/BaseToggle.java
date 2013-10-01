123a7b1fb422ac45f46a61977c92aeedae3cacd8
Jubakuba <Jubakuba@gmail.com>
2013-10-01 14:40:55 -0600

    SystemUI: Toggle Vibrate Duration
    Increase to a more standard UI-touch
     25 (from 10) duration.
    
    Change-Id: I4cdd45f2c5b85497771ff2dc57dbbcf4bc7348fb

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/BaseToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/BaseToggle.java
@@ -136,7 +136,7 @@ public abstract class BaseToggle
 
     protected final void vibrateOnTouch() {
         if (mTactileFeedbackEnabled &&  mVibratePref && vib != null ) {
-            vib.vibrate(10);
+            vib.vibrate(25);
         }
     }
 
