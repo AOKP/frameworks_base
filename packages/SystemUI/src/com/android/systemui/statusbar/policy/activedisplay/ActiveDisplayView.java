8d00e6eea8b49121a4bb0903961d757757f38d7d
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-10-20 11:07:12 +0200

    Active Display: send unlock broadcast
    This will unregister the receiver and fix the memory leak.
    
    Change-Id: Ib99c27c6ad57eaf14d7e9f1adf2a651133ed2503
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -198,6 +198,7 @@ public class ActiveDisplayView extends FrameLayout {
                 mNotification = null;
                 hideNotificationView();
                 if (!mKeyguardManager.isKeyguardSecure()) {
+                    sendUnlockBroadcast();
                     // This is a BUTT ugly hack to allow dismissing the slide lock
                     Intent intent = new Intent(mContext, DummyActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -211,6 +212,7 @@ public class ActiveDisplayView extends FrameLayout {
             } else if (target == OPEN_APP_TARGET) {
                 hideNotificationView();
                 if (!mKeyguardManager.isKeyguardSecure()) {
+                    sendUnlockBroadcast();
                     try {
                         // Dismiss the lock screen when Settings starts.
                         ActivityManagerNative.getDefault().dismissKeyguardOnNextActivity();
@@ -221,6 +223,12 @@ public class ActiveDisplayView extends FrameLayout {
             } else if (target == DISMISS_TARGET) {
                 dismissNotification();
             }
+        }
+
+        private void sendUnlockBroadcast() {
+            Intent u = new Intent();
+            u.setAction("com.android.lockscreen.ACTION_UNLOCK_RECEIVER");
+            mContext.sendBroadcastAsUser(u, UserHandle.ALL);
         }
 
         public void onTargetChange(final View v, final int target) {
