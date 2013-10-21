3b5472f8a4a27ed46d5afd8a30f33ecc7fab9605
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-10-21 05:45:00 +0200

    Active Display: Catch possible NPE during handleDismissNotification()
    * AD doesn't register if notification has been cleared on different device
      (computer) so catch the NPE that would ensue
    
    Change-Id: Ibdb7f526d2c709ebf7a888de717247b7a6114e3e
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -660,6 +660,7 @@ public class ActiveDisplayView extends FrameLayout {
                     mNotification.getPackageName(), mNotification.getTag(),
                     mNotification.getId());
         } catch (RemoteException e) {
+        } catch (NullPointerException npe) {
         }
         mNotification = getNextAvailableNotification();
         if (mNotification != null) {
