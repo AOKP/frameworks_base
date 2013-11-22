daa47cd9b91add14431eaa21906578a83d4ce104
Anu6is <curtis.mayers@gmail.com>
2013-11-22 05:55:06 -0600

    (WIP) [1/2] Frameworks: Optional statusbar battery icons
        * Circle battery and circle battery percent (from CM <Sven Dawitz>)
        * Battery bar percent from Google
        * Quicksetting battery tile reflects status icon (from SlimROM <kufikugel>)
    
    Part 2: packages_apps_Settings
    
    Patch set 2: clean white spaces
    Patch set 3: fix conflicts
    
    Signed-off-by: Anu6is <curtis.mayers@gmail.com>
    
    Change-Id: Ic4056d08874672263444152813a4a8e805a4abf3
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/quick_settings_tile_battery.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
    	wifi/java/android/net/wifi/WifiChannel.aidl
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -16,6 +16,7 @@
 
 package com.android.systemui;
 
+import android.database.ContentObserver;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -25,6 +26,7 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.database.ContentObserver;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
@@ -35,6 +37,7 @@ import android.graphics.Typeface;
 import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
@@ -194,7 +197,6 @@ public class BatteryMeterView extends View implements DemoMode {
 
     public BatteryMeterView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-
         final Resources res = context.getResources();
         TypedArray levels = res.obtainTypedArray(R.array.batterymeter_color_levels);
         TypedArray colors = res.obtainTypedArray(R.array.batterymeter_color_values);
@@ -207,8 +209,6 @@ public class BatteryMeterView extends View implements DemoMode {
         }
         levels.recycle();
         colors.recycle();
-        mShowPercent = ENABLE_PERCENT && 0 != Settings.System.getInt(
-                context.getContentResolver(), "status_bar_show_battery_percent", 0);
 
         mWarningString = context.getString(R.string.battery_meter_very_low_overlay_symbol);
 
@@ -226,7 +226,7 @@ public class BatteryMeterView extends View implements DemoMode {
 
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mTextPaint.setColor(0xFFFFFFFF);
-        Typeface font = Typeface.create("sans-serif-condensed", Typeface.NORMAL);
+        Typeface font = Typeface.create("sans-serif", Typeface.BOLD);
         mTextPaint.setTypeface(font);
         mTextPaint.setTextAlign(Paint.Align.CENTER);
 
@@ -243,6 +243,8 @@ public class BatteryMeterView extends View implements DemoMode {
         mBoltPaint.setColor(res.getColor(R.color.batterymeter_bolt_color));
         mBoltPoints = loadBoltPoints(res);
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        updateSettings();
     }
 
     private static float[] loadBoltPoints(Resources res) {
@@ -369,6 +371,7 @@ public class BatteryMeterView extends View implements DemoMode {
                     (SINGLE_DIGIT_PERCENT ? 0.75f
                             : (tracker.level == 100 ? 0.38f : 0.5f)));
             mTextHeight = -mTextPaint.getFontMetrics().ascent;
+            mTextPaint.setShadowLayer(0.7f,1,1, Color.BLACK);
 
             final String str = String.valueOf(SINGLE_DIGIT_PERCENT ? (level/10) : level);
             final float x = mWidth * 0.5f;
@@ -405,7 +408,7 @@ public class BatteryMeterView extends View implements DemoMode {
         }
     }
 
-    private void updateSettings() {
+    public void updateSettings() {
         boolean enabled = Settings.AOKP.getBoolean(mContext.getContentResolver(),
                                 Settings.AOKP.HIDE_BATTERY_ICON, false);
         if (enabled) {
@@ -413,6 +416,15 @@ public class BatteryMeterView extends View implements DemoMode {
         } else {
             setVisibility(View.VISIBLE);
         }
+        int batteryStyle = Settings.System.getInt(getContext().getContentResolver(),
+                                Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+
+        mShowPercent = batteryStyle == 1;
+        boolean show = (batteryStyle == 0 || mShowPercent);
+
+        setVisibility(show ? View.VISIBLE : View.GONE);
+        postInvalidate();
+
     }
 
 }
