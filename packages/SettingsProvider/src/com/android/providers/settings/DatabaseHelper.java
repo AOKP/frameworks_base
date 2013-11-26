64fa7cd1271102ba1ae94b8a389f62275bc5e0f7
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-11-26 19:54:44 +0100

    AOKP Settings
    First try on adding a seperate AOKP settings table.
    
    Patch Set 2: Fix Bootloop
    
    Change-Id: Ia058c31eab71be47f85e225e763218500b523ae6
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -72,7 +72,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 98;
+    private static final int DATABASE_VERSION = 99;
 
     private Context mContext;
     private int mUserHandle;
@@ -82,11 +82,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_SYSTEM = "system";
     private static final String TABLE_SECURE = "secure";
     private static final String TABLE_GLOBAL = "global";
+    private static final String TABLE_AOKP = "aokp";
 
     static {
         mValidTables.add(TABLE_SYSTEM);
         mValidTables.add(TABLE_SECURE);
         mValidTables.add(TABLE_GLOBAL);
+        mValidTables.add(TABLE_AOKP);
         mValidTables.add("bluetooth_devices");
         mValidTables.add("bookmarks");
 
@@ -137,6 +139,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("CREATE INDEX globalIndex1 ON global (name);");
     }
 
+    private void createAOKPTable(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE aokp (" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT UNIQUE ON CONFLICT REPLACE," +
+                "value TEXT" +
+                ");");
+        db.execSQL("CREATE INDEX aokpIndex1 ON aokp (name);");
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE system (" +
@@ -147,6 +158,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("CREATE INDEX systemIndex1 ON system (name);");
 
         createSecureTable(db);
+
+        createAOKPTable(db);
 
         // Only create the global table for the singleton 'owner' user
         if (mUserHandle == UserHandle.USER_OWNER) {
@@ -1559,6 +1572,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             upgradeVersion = 98;
         }
 
+        if (upgradeVersion == 98) {
+            db.beginTransaction();
+            try {
+                createAOKPTable(db);
+                db.setTransactionSuccessful();
+            } finally {
+                db.endTransaction();
+            }
+            upgradeVersion = 99;
+        }
+
         // *** Remember to update DATABASE_VERSION above!
 
         if (upgradeVersion != currentVersion) {
@@ -1577,6 +1601,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             db.execSQL("DROP INDEX IF EXISTS bookmarksIndex1");
             db.execSQL("DROP INDEX IF EXISTS bookmarksIndex2");
             db.execSQL("DROP TABLE IF EXISTS favorites");
+            db.execSQL("DROP TABLE IF EXISTS aokp");
+            db.execSQL("DROP INDEX IF EXISTS aokpIndex1");
             onCreate(db);
 
             // Added for diagnosing settings.db wipes after the fact
@@ -1959,6 +1985,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         if (mUserHandle == UserHandle.USER_OWNER) {
             loadGlobalSettings(db);
         }
+        loadAokpSettings(db);
     }
 
     private void loadSystemSettings(SQLiteDatabase db) {
@@ -2028,6 +2055,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private void loadDefaultHapticSettings(SQLiteStatement stmt) {
         loadBooleanSetting(stmt, Settings.System.HAPTIC_FEEDBACK_ENABLED,
                 R.bool.def_haptic_feedback);
+    }
+
+    private void loadAokpSettings(SQLiteDatabase db) {
+        SQLiteStatement stmt = null;
+        try {
+            //stmt = db.compileStatement("INSERT OR IGNORE INTO aokp(name,value)"
+            //        + " VALUES(?,?);");
+            // TODO: Preload any required default values
+        } finally {
+            if (stmt != null) stmt.close();
+        }
     }
 
     private void loadSecureSettings(SQLiteDatabase db) {
