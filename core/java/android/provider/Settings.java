0da71e8893a7bcd9c1b81f775dbe72a5225d43b5
Lars Greiss <kufikugel@googlemail.com>
2013-12-01 14:14:38 +0100

    Fw_base - Less notifications sound (1/2)
    add back the 4.3 feature like it was.
    User can setup how often notification sound
    for an app comes trough so that
    it does not annoy to much.
    
    Ported to AOKP Oreo
    
    Change-Id: I3d7b2ef2e2ff99b7c23880626e0268ec4c5d8543
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4116,6 +4116,13 @@ public final class Settings {
         public static final String SCREENSHOT_SOUND = "screenshot_sound";
 
         /**
+         * Whether to mute annoying notifications
+         * @hide
+         */
+        public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD =
+                "mute_annoying_notifications_threshold";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
