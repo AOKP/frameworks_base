51e6f4d5ea690b5edd91eec9973280229986f706
Roman Birg <romanbirg@gmail.com>
2013-12-28 16:38:07 -0500

    SystemUI: navigation buttons WIP
    Change-Id: Ie3ba3de6afec3de93e8f4a4c59fcf89d5c6c8941
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/statusbar/navbar/NavButton.java
@@ -0,0 +1,15 @@
+package com.android.systemui.statusbar.navbar;
+
+import android.graphics.drawable.Drawable;
+
+import com.android.internal.util.aokp.AwesomeConstants;
+
+/**
+ * Created by roman on 12/28/13.
+ */
+public class NavButton {
+    String mAction; // an AwesomeConstant or app intent
+    Drawable mIcon;
+
+
+}
