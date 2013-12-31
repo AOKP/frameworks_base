cecd57db2909e6f13a14b89fe26c78a15b2448f3
Steve Spear <stephen.k.spear@gmail.com>
2013-12-30 19:26:22 -0500

    Ribbons Draft
    Change-Id: I9c9687cab1b5685b2859ed09228bae132b3500e8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6660,6 +6660,38 @@ public final class Settings {
         }
 
         /**
+         * @hide
+         */
+        public static final String[] AOKP_LEFT_RIBBON = new String[] {
+            "left_enabled",
+            "left_ribbon_items",
+            "left_ribbon_long_items",
+            "left_handle_weight",
+            "left_handle_height",
+            "left_handle_opacity",
+            "left_handle_vibrate",
+            "left_handle_location",
+            "left_handle_long_swipe",
+            "left_handle_long_press",
+        };
+
+        /**
+         * @hide
+         */
+        public static final String[] AOKP_RIGHT_RIBBON = new String[] {
+            "right_enabled",
+            "right_ribbon_items",
+            "right_ribbon_long_items",
+            "right_handle_weight",
+            "right_handle_height",
+            "right_handle_opacity",
+            "right_handle_vibrate",
+            "right_handle_location",
+            "right_handle_long_swipe",
+            "right_handle_long_press",
+        };
+
+        /**
          * Whether to enable quiet hours.
          * @hide
          */
