3e112765cb31d682dfae782280f4c878d8fcb65a
garwedgess <garethwilliams21@gmail.com>
2014-01-24 13:48:27 -0500

    [1/4] Base: Breathing missedcall/sms/voicemail
    Change-Id: Idcedcea65999047329df9aa987e5c770c2ab70fa

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7090,6 +7090,24 @@ public final class Settings {
          * @hide
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
+         * Give SMS notifications a breathing effect
+         * @hide
+         */
+	public static final String KEY_SMS_BREATH = "sms_breath";
+
+        /**
+         * Give Missed call notifications a breathing effect
+         * @hide
+	 */
+        public static final String KEY_MISSED_CALL_BREATH = "missed_call_breath";
+
+         /**
+          * Give Voicemail notifications a breathing effect
+          * @hide
+          */
+        public static final String KEY_VOICEMAIL_BREATH = "voicemail_breath";
     }
 
     /**
