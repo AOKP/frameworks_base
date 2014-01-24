244ce7bc5d2d6aa51592e07d4616ba8e9dcdfa8f
garwedgess <garethwilliams21@gmail.com>
2014-01-24 13:48:27 -0500

    [1/4] Base: Breathing missedcall/sms/voicemail
    Change-Id: Idcedcea65999047329df9aa987e5c770c2ab70fa

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2420,6 +2420,24 @@ public final class Settings {
          * @hide
          */
         public static final String POINTER_SPEED = "pointer_speed";
+                
+        /**
+         * Give SMS notifications a breathing effect
+         * @hide
+         */
+	public static final String KEY_SMS_BREATH = "sms_breath";
+
+        /**
+         * Give Missed call notifications a breathing effect
+         * @hide
+	 */
+         public static final String KEY_MISSED_CALL_BREATH = "missed_call_breath";
+
+         /**
+          * Give Voicemail notifications a breathing effect
+          * @hide
+          */
+          public static final String KEY_VOICEMAIL_BREATH = "voicemail_breath";
 
         /**
          * I am the lolrus.
