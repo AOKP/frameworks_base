206bcba520dff45c93036b26dc447b2e984e3178
David96 <xd_e-mail@gmx.de>
2014-02-01 17:56:38 +0100

    Lockscreen Notifications [1/2]
    My try on making notifications easily accessable from lockscreen.
    Credits to ChameleonOS, I got the notification listener permission and some small other stuff from their ActiveDisplay implementation.
    
    Lockscreen Notifications allow you to manage all your notifications directly on the lockscreen.
    
    Features:
    - Pocket mode turn screen on when phone is pulled out of pocket and turn it off when phone gets back in pocket
    - Hide low priority notifications
    - Hide non-clearable notifications
    - Allow dismissing even non-clearable notifications from lockscreen
    - Just turn screen on when there's currently a notification pending
    - Toggle between expanded and normal notification view by longpressing
    - Wake on new notification
    - Configure notification count displayed at once
    - Privacy Mode: disable showing notification content.
    - Blacklist apps - thanks to ChameleonOS for the original implementation and @Der-Schubi for bringing it to lockscreen notifications
    - Show all/dismiss all button
    
    This should still be considered as beta, even if it seems to work very well for me.
    
    Overlay setting:
    dynamic width is disabled by default, to enable it for a specific device, add
    <bool name="config_lnDynamicWidth">true</bool>
    to yourdevicetree/overlay/frameworks/base/packages/Keyguard/res/values/config.xml
    
    Change-Id: Ife8cbe1ef86691c391de061964bf4d14be03a59f
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2873,6 +2873,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (false) Log.v(TAG, "updateResources");
     }
 
+    @Override
+    public void setButtonDrawable(int buttonId, int iconId) {
+        mNavigationBarView.setButtonDrawable(buttonId, iconId);
+    }
     //
     // tracing
     //
