b4345dfa33c45fc551c5f5e492dd4b9643bb040d
Arnav Gupta <championswimmer@aokp.co>
2014-02-04 04:47:18 +0530

    immersive mode: use sticky mode, do not hide action bar
    Change-Id: I2baa394339923483825a826da893c32889d58f58
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -5504,11 +5504,9 @@ public class Activity extends ContextThemeWrapper
         public void run() {
             mWindow.getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     };
 }
