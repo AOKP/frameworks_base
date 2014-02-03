7ad9e9ae1ed51c0c814691e9fb63ba0f1a156f97
Arnav Gupta <championswimmer@aokp.co>
2014-02-04 04:47:18 +0530

    immersive mode: use sticky immersive mode
    Change-Id: I2baa394339923483825a826da893c32889d58f58
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -5508,7 +5508,7 @@ public class Activity extends ContextThemeWrapper
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     };
 }
