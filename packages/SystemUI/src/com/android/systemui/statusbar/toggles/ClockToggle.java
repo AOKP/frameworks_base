3a17642c7ed25634bd60c2e12d4b7f99bbea70f7
sethyx <sethyx@gmail.com>
2014-03-07 22:51:11 +0100

    Add implementation for Clock (Alarm) Toggle
    PS1: Remove extra space
    
    Change-Id: I8d571082f5e264a1560e7e549bb555de45202866
    Signed-off-by: sethyx <sethyx@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ClockToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ClockToggle.java
@@ -1,11 +1,17 @@
 
 package com.android.systemui.statusbar.toggles;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.UserHandle;
+import android.provider.AlarmClock;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.TextView;
+import android.text.TextUtils;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsTileView;
@@ -15,18 +21,36 @@ public class ClockToggle extends BaseToggle {
     @Override
     public void init(Context c, int style) {
         super.init(c, style);
+        onAlarmChanged(false);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_ALARM_CHANGED);
+        registerBroadcastReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                onAlarmChanged(intent.getBooleanExtra("alarmSet", false));
+            }
+        }, filter);
+    }
+
+    void onAlarmChanged(boolean enabled) {
+        boolean state = enabled;
+        final String alarmText = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.NEXT_ALARM_FORMATTED,
+                UserHandle.USER_CURRENT);
+        state =! TextUtils.isEmpty(alarmText); // this may look silly, but is necessary
+        setInfo(state ? alarmText : mContext.getString(
+                R.string.quick_settings_clock_alarm_off), getDefaultIconResId());
+        scheduleViewUpdate();
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent("android.intent.action.MAIN");
-        intent.setComponent(ComponentName
-                .unflattenFromString("com.android.deskclock.AlarmProvider"));
-        intent.addCategory("android.intent.category.LAUNCHER");
+        Intent intent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         vibrateOnTouch();
         collapseStatusBar();
         dismissKeyguard();
-        startActivity(intent);
+        mContext.startActivityAsUser(intent, new UserHandle(UserHandle.USER_CURRENT));
     }
 
     @Override
