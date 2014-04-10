3d50454d4b339d4ab3db1eb0671d1b7cc94a91e3
BigBrother1984 <stevewatersy@gmail.com>
2014-04-10 18:07:26 +0200

    Fw_Base - Floating Window [1/2]
    Floating window contributors:
    @drcmda
    @beerbong
    @D4rKn3sSyS
    @Squadzone
    
    @LorDClockaN: Chrome floating fix
    
    VRToxin work:
    
    NL-BlackDragon:* Force immersive mode during float mode
                   * Kill floating activities running in the same thread
    when tapping outside.
                   * Fix showing maximize button when it can't be used
                   * Fix immersive mode snap points
                   * Fix minimize/maximize button behavior
                   * Fix minimize/maximize buttons coloring
                   * When closing floating window also kill task
                   * Add app title to Floating's title bar
                   * Added some tinted code by XplodWild.
                   * Add floating window AppCircleSideBar support.
                   * Stock recents float mode
                   * Hide notification float button when disabled
                   * only show maximize button when in normal
                     floating window view and only show the minimize
                     button when in full screen view
    
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
    
    rogersb11:     * For fucks sake, convert new google multiwindow to
    previous float mode keeping some improvements
                   * Add old policies and preload needed classes for
    PhoneWindow
                   * Change the way PWM handles window calls/functions
                   * Full Marshmallow adaption
                   * Add floating intent to extra features AppSideBar, Slim
    recents
                   * Allow using new google MULTI window button in stock
    recents and give option for single float mode as well
                   * Slim actions float mode
                   * Floating and slim floats tiles and buttons and slim
    action
                   * Fix spacing of buttons in recents header
                   * Fix nonstop fatal exception on creating windows
    
    Screw'd:
    Modified for Screw'd AOSP
    QS Tiles modified for our QSTuner implementation
    
    AICPfy:
    Make it work with AICP source
    Actions, tile, overrides, etc etc...
    
    Signed-off-by: rogersb11 <brettrogers11@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Change-Id: I81ba0bfaf8e6f29388e07beb78197dd316a11de7

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -2181,7 +2181,8 @@ public final class ActivityStackSupervisor implements DisplayListener {
                                 intentActivity.task.setTaskToReturnTo(HOME_ACTIVITY_TYPE);
                             }
                             options = null;
-                        }
+                            movedToFront = true;
+                        }             
                     }
                     if (!movedToFront) {
                         if (DEBUG_TASKS) Slog.d(TAG_TASKS, "Bring to front target: " + targetStack
