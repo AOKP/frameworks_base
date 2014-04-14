1ed94e6b309ecee4c05f9bb1d1b4aa47c015051e
Danesh Mondegarian <daneshm90@gmail.com>
2014-04-13 18:14:05 -0700

    Keyguard : Fix album art sticking after playback is stopped
    newIsNull should be populated by what is currently being set and not by
    what the user's custom background is, since otherwise you end up a in state
    where the user has no custom background set, music stops, no album art via arg is provided and you simply return.
    
    Change-Id: Ifaea1cae176faaba6b32a711071f3b75ccacdd63

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -324,7 +324,7 @@ public class KeyguardViewManager {
                 if (old == null && d == null && mUserBackground == null) {
                     return;
                 }
-                boolean newIsNull = mUserBackground == null;
+                boolean newIsNull = false;
                 if (old == null) {
                     old = new ColorDrawable(BACKGROUND_COLOR);
                     old.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.SRC_OVER);
@@ -335,6 +335,7 @@ public class KeyguardViewManager {
                 // no user wallpaper set
                 if (d == null) {
                     d = new ColorDrawable(BACKGROUND_COLOR);
+                    newIsNull = true;
                 }
                 d.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.SRC_OVER);
                 computeCustomBackgroundBounds(d);
