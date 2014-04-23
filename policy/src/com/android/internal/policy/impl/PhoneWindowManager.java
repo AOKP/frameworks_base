ec7c03fd77ace6c56ecc8c3a2cd13f4ee8e66615
kunleiz <quic_kunleiz@codeaurora.org>
2014-04-23 17:22:50 +0800

    audio: Volume can't adjust during HFP call when screen is off.
    PhoneWindowManager don't pass the key to the application when screen is
    off. In handleVolumeKey(), the STREAM_VOICE_CALL volume was set, so the
    STREAM_BLUETOOTH_SCO volume can't be adjusted.
    Add a judgement to choose the correct stream to adjust when screen is off.
    
    CRs-Fixed: 649548
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: I1ab06617d59b9bf2eabed9ed8b6892bc20db75b9

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4452,6 +4452,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
         final boolean canceled = event.isCanceled();
         final int keyCode = event.getKeyCode();
+        AudioManager audioManager = (AudioManager) mContext.getSystemService(
+                Context.AUDIO_SERVICE);
 
         final boolean isInjected = (policyFlags & WindowManagerPolicy.FLAG_INJECTED) != 0;
 
@@ -4631,7 +4633,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                                     && (result & ACTION_PASS_TO_USER) == 0) {
                                 // If we are in call but we decided not to pass the key to
                                 // the application, handle the volume change here.
-                                handleVolumeKey(AudioManager.STREAM_VOICE_CALL, keyCode);
+                                if(audioManager.isBluetoothScoOn()) {
+                                    handleVolumeKey(AudioManager.STREAM_BLUETOOTH_SCO, keyCode);
+                                } else {
+                                    handleVolumeKey(AudioManager.STREAM_VOICE_CALL, keyCode);
+                                }
                                 break;
                             }
                         } catch (RemoteException ex) {
