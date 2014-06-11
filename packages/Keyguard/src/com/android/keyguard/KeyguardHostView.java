05f4191f4be8ad90a1484362b035baca8761349a
Jonathan Steadman <xxwhitehawk@gmail.com>
2014-06-11 22:28:47 +0200

    framework: option to enable/disable menu unlock (1/2)
    Change-Id: I5e4b82148ff434ee8a56bd41a46be2f4e085f155
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
@@ -1705,7 +1705,9 @@ public class KeyguardHostView extends KeyguardViewBase {
         final boolean configDisabled = res.getBoolean(R.bool.config_disableMenuKeyInLockScreen);
         final boolean isTestHarness = ActivityManager.isRunningInTestHarness();
         final boolean fileOverride = (new File(ENABLE_MENU_KEY_FILE)).exists();
-        return !configDisabled || isTestHarness || fileOverride;
+        final boolean menuOverride = Settings.AOKP.getBoolean(getContext().getContentResolver(),
+                Settings.AOKP.LOCKSCREEN_MENU_UNLOCK, true);
+        return (!configDisabled || isTestHarness || fileOverride) && menuOverride;
     }
 
     public void goToWidget(int appWidgetId) {
