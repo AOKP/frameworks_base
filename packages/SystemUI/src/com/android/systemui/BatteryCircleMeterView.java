a3871e446beb88b5f7818e6a8748d50a3e2cb68e
kecinzer <kecinzer@gmail.com>
2014-06-27 08:58:27 +0200

    Circle battery CM style
    Change-Id: I6a4febee155d9d7a08bec9aae4a1fed6f63210c0
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
@@ -294,9 +294,7 @@ public class BatteryCircleMeterView extends ImageView {
         mPaintSystem = new Paint(mPaintFont);
         mPaintRed = new Paint(mPaintFont);
 
-        // could not find the darker definition anywhere in resources
-        // do not want to use static 0x404040 color value. would break theming.
-        mPaintGray.setColor(res.getColor(R.color.darker_gray));
+        mPaintGray.setColor(res.getColor(com.android.systemui.R.color.batterymeter_frame_color));
         mPaintRed.setColor(res.getColor(R.color.holo_red_light));
 
         // font needs some extra settings
@@ -346,10 +344,10 @@ public class BatteryCircleMeterView extends ImageView {
 
         mPaintFont.setTextSize(mCircleSize / 2f);
 
-        float strokeWidth = mCircleSize / 7f;
+        float strokeWidth = mCircleSize / 6.5f;
         mPaintRed.setStrokeWidth(strokeWidth);
         mPaintSystem.setStrokeWidth(strokeWidth);
-        mPaintGray.setStrokeWidth(strokeWidth / 3.5f);
+        mPaintGray.setStrokeWidth(strokeWidth);
         // calculate rectangle for drawArc calls
         int pLeft = getPaddingLeft();
         mRectLeft = new RectF(pLeft + strokeWidth / 2.0f, 0 + strokeWidth / 2.0f, mCircleSize
@@ -363,7 +361,8 @@ public class BatteryCircleMeterView extends ImageView {
         mPaintFont.getTextBounds("MM", 0, "MM".length(), bounds);
         mTextLeftX = mCircleSize / 2.0f + getPaddingLeft();
         mTextRightX = mTextLeftX + off;
-        mTextY = mCircleSize / 2.0f + (bounds.bottom - bounds.top) / 2.0f;
+        mTextY = mCircleSize / 2.0f + (bounds.bottom - bounds.top) / 2.0f
+                - strokeWidth / 2.0f + getResources().getDisplayMetrics().density;
 
         // force new measurement for wrap-content xml tag
         onMeasure(0, 0);
