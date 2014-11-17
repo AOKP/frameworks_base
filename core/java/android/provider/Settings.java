cd3813b49a08403d7c4b41628d7f93d5ec328c67
Danesh Mondegarian <daneshm90@gmail.com>
2014-11-16 22:26:45 -0600

    Fw_base - Forward port Long press back to kill app + timeout(1/2)
    Change-Id: Ia88d8a2753e203afb8d6117a643c33127f991c4a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7055,6 +7055,18 @@ public final class Settings {
         public static final String SCREENSAVER_DEFAULT_COMPONENT = "screensaver_default_component";
 
         /**
+         * Whether to allow killing of the foreground app by long-pressing the Back button
+         * @hide
+         */
+        public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
+        /**
+         * Timeout of long-pressing the Back button
+         * @hide
+         */
+        public static final String KILL_APP_LONGPRESS_TIMEOUT = "kill_app_longpress_timeout";
+
+        /**
          * The default NFC payment component
          * @hide
          */
