4520dee1683eba11a645decf2b2b2e2ddc887377
Roman Birg <roman@cyngn.com>
2014-12-11 17:41:40 -0800

    SystemUI: add Weather display to status bar header
    Change-Id: Ic2fea861c36f405d8a0e64cc9224f47088ac6351
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    frameworks: fix make updateapi for weather setting
    
    Change-Id: I3a23cd432138e63e037b427ceaf62a87627dfde6
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4184,6 +4184,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
 
         /**
+         * Boolean value on whether to show weather in the statusbar
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
