d5bd2728d3f0f90655fe8c06b10a27d2f58418f4
Lars Greiss <kufikugel@googlemail.com>
2014-12-16 17:25:58 +0000

    Fw_base - Add ability to disable bar color in battery saver mode (1/2)
    Change-Id: I8752509efaa0baf8a9f02d791f222bf619088349

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3808,6 +3808,13 @@ public final class Settings {
                 "proximity_auto_speaker_incall_only";
 
         /**
+         * Whether to enable status and navigation bar color in battery saver mode.
+         *
+         * @hide
+         */
+        public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
