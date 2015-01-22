bad76f8324ce9dbb5e826d6b6d139b519147bfc8
Roman Birg <roman@cyngn.com>
2015-01-22 11:21:56 -0800

    System Profiles in QS Tiles
    Contains also commit by Joey Rizzoli:
    
    SystemUI: Profiles tile should require authentication
    
    A profile with the option to disable secure lockscreen could be
    used to bypass the lockscreen security.
    Require the user to unlock their device when tapping on the profile
    quick tile.
    
    BUGBASH-1095
    
    Change-Id: I2a438af301212241533b969bf2c6c8390ef09cbc

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -46,6 +46,7 @@ import com.android.systemui.qs.tiles.NavigationBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.PowerMenuTile;
+import com.android.systemui.qs.tiles.ProfilesTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.ROMControlTile;
@@ -81,6 +82,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("rotation")) return new RotationLockTile(mHost);
         else if (tileSpec.equals("flashlight")) return new FlashlightTile(mHost);
         else if (tileSpec.equals("location")) return new LocationTile(mHost);
+        else if (tileSpec.equals("profiles")) return new ProfilesTile(mHost);
         else if (tileSpec.equals("cast")) return new CastTile(mHost);
         else if (tileSpec.equals("hotspot")) return new HotspotTile(mHost);
         else if (tileSpec.equals("user")) return new UserTile(mHost);
