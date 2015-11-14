db828432b098fe80f5347b3c71f7802e645cc8ce
LorDClockaN <davor@losinj.com>
2015-11-14 21:19:17 +0100

    Fw_base - Toast icon switch [1/2]
    Change-Id: I59ffd10f20a2ae9491f76380ec9af641e0844928

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3815,6 +3815,13 @@ public final class Settings {
         public static final String BATTERY_SAVER_MODE_COLOR = "battery_save_mode_color";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
