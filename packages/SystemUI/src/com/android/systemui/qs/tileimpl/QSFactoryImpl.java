e038ad5fbdd274d9bac6ed5e9d5dfb55cb08c993
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-11-29 06:34:41 +0000

    Fw_base - [QUICK SETTINGS] Add Reboot/Recovery QS Tile
    Change-Id: I2c7956b7201e2103420fc7254051141807024fc4
    Signed-off-by: villarleg <santialovera20@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -131,6 +132,8 @@ public class QSFactoryImpl implements QSFactory {
 			// AOKP
             case "screenshot":
                 return new ScreenshotTile(mHost);
+            case "reboot":
+                return new RebootTile(mHost);
         }
 
         // Intent tiles.
