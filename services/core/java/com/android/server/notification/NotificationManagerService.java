388a2e554b85173997a768eca9f0c2c3e0f33732
Roman Birg <roman@cyngn.com>
2015-12-23 14:02:52 -0800

    NotificationManager: always restore identity on notification error
    Change-Id: I3fb369c34098c366c3c943da0d7c7c64127387c8
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -706,8 +706,9 @@ public class NotificationManagerService extends SystemService {
                         "Bad notification posted from package " + pkg
                         + ": " + message);
             } catch (RemoteException e) {
+            } finally {
+                Binder.restoreCallingIdentity(ident);
             }
-            Binder.restoreCallingIdentity(ident);
         }
 
         @Override
