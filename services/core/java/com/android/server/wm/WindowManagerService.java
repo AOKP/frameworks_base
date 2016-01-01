427972779944401151d7d731864e30e36176402f
Lars Greiss <kufikugel@googlemail.com>
2016-01-01 15:56:53 -0700

    Fw_base - Add SlimRecents [1/2]
    Marshmallow bring up by @freak97
    
    Change-Id: I9da0aa70d957f136e2298445aedff9c7a9d20f04
    Signed-off-by: STELIX <ssspinni@gmail.com>
    Signed-off-by: GalaticStryder <icarohoff@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -12120,4 +12120,10 @@ public class WindowManagerService extends IWindowManager.Stub
             }
         }
     }
+
+    /* @hide */
+    @Override
+    public int getSystemUIVisibility() {
+        return mLastStatusBarVisibility;
+    }
 }
