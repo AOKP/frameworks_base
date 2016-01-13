3ad7aa57a19f9aceb772d3bce0a8ddd1cf21d42e
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-01-13 09:56:19 -0700

    Fw_base - Notifications: Custom colors (1/2)
    ("normal", heads up and lock screen notifications)
    
    - Media background color mode:
    - Never colorize the background
    - Colorize the background if the notification doesn`t include a own
    color
    - Always colorize the background
    - App icon background color mode:
    - Never colorize the background
    - Colorize the background if the notification doesn`t include a own
    color
    - Always colorize the background
    - App icon color mode:
    - Never colorize the background
    - Colorize the icon if the icon is an greyscale icon
    - Always colorize the background
    - Background color
    - Background color, (on long click)
    - App icon background color
    - Text color
    - Icon colors,
    (app-, action- and media action icons)
    
    Fixes and additions:
    - Use different colors for high/normal or minor priority
    (minor priority color is a darker/lighter color
    of the high/normal color)
    - Don`t use greyscale icons when dozing
    - Show the app icon background when dozing,
    (there is no color inverting, using a dark/black background
    will hide the background)
    
    Known bugs:
    
    Media notifications:
    - Some media notifications doesn`t respect the choosen action icon color
    and text colors
    (e.g. PowerAmp. Notifications from Google Play Music and Eleven
    are working without any problems)
    
    Notifications (general):
    - Some notifications doesn`t respect a new text color,
    (if the notification is already present,
    new notifications are always using the "right" text color,
    e.g. GMail, Hangouts, ... seems it`s a similar issue like the one I
    fixed with
    the "Notifications: Fix dark text on dark background" commit,
    but unfortunately this fix doesn`t work in this case)
    
    May be there are more bugs with notifications from other apps,
    or scenarios I didn`t tested.
    
    Change-Id: Ia625ba27cd417538869397ea7b1a662d0bfaf559

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4261,6 +4261,66 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";
 
+         /**
+         * Colorize the media notifications background
+         * 0: never
+         * 1: if the notification doesn`t include an own color
+         * 2: always
+         * default: 0
+         * @hide
+         */
+        public static final String NOTIFICATION_MEDIA_BG_MODE = "notification_media_bg_mode";
+
+        /**
+         * Colorize the notifications app icon background
+         * 0: never
+         * 1: if the notification doesn`t include an own color
+         * 2: always
+         * default: 0
+         * @hide
+         */
+        public static final String NOTIFICATION_APP_ICON_BG_MODE = "notification_app_icon_bg_mode";
+
+       /**
+         * Colorize the notifications app icon
+         * 0: never
+         * 1: if the icon is an greyscale icon
+         * 2: always
+         * default: 0
+         * @hide
+         */
+        public static final String NOTIFICATION_APP_ICON_COLOR_MODE = "notification_app_icon_color_mode";
+
+        /**
+         * Color of the notifications background
+         * @hide
+         */
+        public static final String NOTIFICATION_BG_COLOR = "notification_bg_color";
+
+        /**
+         * Color of the notifications background on longpress
+         * @hide
+         */
+        public static final String NOTIFICATION_GUTS_BG_COLOR = "notification_bg_guts_color";
+
+        /**
+         * Color of the notifications icon background
+         * @hide
+         */
+        public static final String NOTIFICATION_APP_ICON_BG_COLOR = "notification_app_icon_bg_color";
+
+        /**
+         * Color of the notification text
+         * @hide
+         */
+        public static final String NOTIFICATION_TEXT_COLOR = "notification_text_color";
+
+        /**
+         * Color of notification icon
+         * @hide
+         */
+        public static final String NOTIFICATION_ICON_COLOR = "notification_icon_color";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
--- core/java/com/android/internal/util/slim/ColorHelper.java
+++ core/java/com/android/internal/util/slim/ColorHelper.java
@@ -19,6 +19,7 @@ package com.android.internal.util.slim;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
@@ -75,4 +76,63 @@ public class ColorHelper {
         return bmpGrayscale;
     }
 
+    public static int getBlendColor(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float a = Color.alpha(to) * ratio + Color.alpha(from) * inverseRatio;
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.argb((int) a, (int) r, (int) g, (int) b);
+    }
+
+    public static boolean isColorDark(int color) {
+        double a = 1- (0.299 * Color.red(color)
+                + 0.587 * Color.green(color)
+                + 0.114 * Color.blue(color)) / 255;
+        if (a < 0.5) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int getLightenOrDarkenColor(int originalColor) {
+        boolean isOriginalColorDark = isColorDark(originalColor);
+        float factor = isOriginalColorDark ? 0.1f : 0.8f;
+        int a = Color.alpha(originalColor);
+        int r = Color.red(originalColor);
+        int g = Color.green(originalColor);
+        int b = Color.blue(originalColor);
+        int newColor;
+
+        if (isOriginalColorDark) {
+            newColor = Color.argb(a,
+                    (int) ((r * (1 - factor) / 255 + factor) * 255),
+                    (int) ((g * (1 - factor) / 255 + factor) * 255),
+                    (int) ((b * (1 - factor) / 255 + factor) * 255));
+        } else {
+            newColor = Color.argb(a,
+                    Math.max((int) (r * factor), 0),
+                    Math.max((int) (g * factor), 0),
+                    Math.max((int) (b * factor), 0));
+        }
+        return newColor;
+    }
+
+    public static ColorMatrixColorFilter getColorFilter(int color) {
+        float r = Color.red(color) / 255f;
+        float g = Color.green(color) / 255f;
+        float b = Color.blue(color) / 255f;
+
+        ColorMatrix cm = new ColorMatrix(new float[] {
+                r, 0, 0, 0, 0,
+                0, g, 0, 0, 0,
+                0, 0, b, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+        ColorMatrixColorFilter cf = new ColorMatrixColorFilter(cm);
+        return cf;
+    }
 }
