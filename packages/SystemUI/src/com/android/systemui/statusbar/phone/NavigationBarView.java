c6695ed978524eb03945edb9a210727ffb1b89b2
d34d <clark@cyngn.com>
2016-01-15 15:02:47 -0800

    SysUI: Pass themed resources on to NavBarEditor
    This fixes an issue where additional buttons in the nav bar would
    pick up the SystemUI theme instead of the one assigned to the
    navigation bar.  This patch esnures that the correct resources are
    used when updating the buttons.
    
    Change-Id: Ic187e702355bec1b85353229e2c3a92a6eb6b8e1
    TICKET: CYNGNOS-1620

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -280,7 +280,7 @@ public class NavigationBarView extends LinearLayout {
         mDisplay = ((WindowManager)context.getSystemService(
                 Context.WINDOW_SERVICE)).getDefaultDisplay();
 
-        final Resources res = getContext().getResources();
+        final Resources res = getResources();
         mBarSize = res.getDimensionPixelSize(R.dimen.navigation_bar_size);
         mVertical = false;
         mShowMenu = false;
@@ -413,6 +413,9 @@ public class NavigationBarView extends LinearLayout {
                 updateLightsOutResources(container);
             }
         }
+        if (mEditBar != null) {
+            mEditBar.updateResources(res);
+        }
     }
 
     private void updateLightsOutResources(ViewGroup container) {
@@ -436,7 +439,7 @@ public class NavigationBarView extends LinearLayout {
 
     @Override
     public void setLayoutDirection(int layoutDirection) {
-        getIcons(mThemedResources != null ? mThemedResources : getContext().getResources());
+        getIcons(getResources());
 
         super.setLayoutDirection(layoutDirection);
     }
@@ -699,7 +702,7 @@ public class NavigationBarView extends LinearLayout {
         } else {
             mVertical = getWidth() > 0 && getHeight() > getWidth();
         }
-        mEditBar = new NavbarEditor(mCurrentView, mVertical, mIsLayoutRtl);
+        mEditBar = new NavbarEditor(mCurrentView, mVertical, mIsLayoutRtl, getResources());
         updateSettings();
 
         getImeSwitchButton().setOnClickListener(mImeSwitcherClickListener);
@@ -798,7 +801,7 @@ public class NavigationBarView extends LinearLayout {
 
     private String getResourceName(int resId) {
         if (resId != 0) {
-            final android.content.res.Resources res = getContext().getResources();
+            final android.content.res.Resources res = getResources();
             try {
                 return res.getResourceName(resId);
             } catch (android.content.res.Resources.NotFoundException ex) {
@@ -949,9 +952,9 @@ public class NavigationBarView extends LinearLayout {
         }
     }
 
-    // TODO LINK TO THIS ONCE THEMES GOES IN
-    protected void updateResources() {
-        getIcons(mContext.getResources());
+    @Override
+    public Resources getResources() {
+        return mThemedResources != null ? mThemedResources : getContext().getResources();
     }
 
     public class NavBarReceiver extends BroadcastReceiver {
