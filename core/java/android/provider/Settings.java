a437d55082f3f148f8e474c5e74a28bd2628a014
rogersb11 <brettrogers11@gmail.com>
2016-02-02 22:46:10 -0500

    Fw_base - Heads Up Snooze [1/2]
    Change-Id: If8337cff505a45d505153244247c621dc573dbd4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4316,6 +4316,13 @@ public final class Settings {
         public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
 
         /**
+         * Snooze time for heads up
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_SNOOZE_LENGTH_MS = "heads_up_snooze_length_ms";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
