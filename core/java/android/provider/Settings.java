5d1800d6535209b3ee6e617e365fc9c000c81fbb
Alex Cruz <mazdarider23@gmail.com>
2016-02-06 03:59:14 -0500

    Fw_base - Hide Lockscreen clock & date [1/2]
    Before - http://i.imgur.com/XDucx5p.jpg / http://i.imgur.com/GQ4gjn8.png
    After - http://i.imgur.com/m7dnaNB.jpg / http://i.imgur.com/UKjunnW.png
    
    Change-Id: I06b2170f58b002d47f04fdbb5e165ee30cf73157
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4333,6 +4333,18 @@ public final class Settings {
         public static final String STATUS_BAR_AOKP_LOGO_STYLE = "status_bar_aokp_logo_style";
 
         /**
+         * Hide lockscreen date
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_DATE = "hide_lockscreen_date";
+
+        /**
+         * Hide lockscreen clock
+         * @hide
+         */
+        public static final String HIDE_LOCKSCREEN_CLOCK = "hide_lockscreen_clock";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
