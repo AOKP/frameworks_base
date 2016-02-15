18ee065190d5b3d96797b8bc3239a5a087db0d7f
Roman Birg <roman@cyngn.com>
2016-02-15 12:37:14 -0800

    SystemUI: disable QS tile Edit mode on screen off
    Ref: CYNGNOS-1936
    
    Change-Id: I08d4cf6c3613bdc8434c3cd106e6940b0c0a8005
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -5014,6 +5014,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStackScroller.setAnimationsEnabled(false);
         updateVisibleToUser();
         mVisualizerView.setVisible(false);
+        if (mQSTileHost.isEditing()) {
+            mQSTileHost.setEditing(false);
+        }
         if (mLaunchCameraOnFinishedGoingToSleep) {
             mLaunchCameraOnFinishedGoingToSleep = false;
 
