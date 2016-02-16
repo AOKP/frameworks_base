7884334ce54de006b40a44828c275c8f60aec012
d34d <clark@cyngn.com>
2016-02-15 18:58:24 -0800

    SysUI: Wait for keyguard gone before starting activity
    Some apps, I'm looking at you Chrome, don't like to do any work
    if they are being started while the lock screen is shown, even
    if it is being dismissed.  This patch adds another version of
    startActivityDismissingKeyguard() which takes an extra boolean
    parameter that allows to override the afterKeyguardGone behavior.
    Normally that is only true if the resolver will be shown instead
    of launching an app.  Overriding this allows us to get the exact
    behavior we want without altering this behavior under normal
    circumstances.
    
    Change-Id: Ia804c7a5a73f2a4ad45593bcfcc192fedddf8d92
    TICKET: CYNGNOS-2044

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1244,7 +1244,8 @@ public class NotificationPanelView extends PanelView implements
                         @Override
                         public void run() {
                             mStatusBar.showKeyguard();
-                            mStatusBar.startActivity(intent, true);
+                            mStatusBar.startActivityDismissingKeyguard(intent, false, true, true,
+                                    null);
                         }
                     });
                 }
