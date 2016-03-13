bebf0b70c495ee6508d920a0a09ef27b1d107458
ezio84 <brabus84@gmail.com>
2016-03-13 15:27:11 +0100

    Fw_base - Speed up the notification panel pulldown animation when called externally
    https://code.google.com/p/android/issues/detail?id=189826#c4
    To avoid the animation glitch, this needs also:
    http://review.cyanogenmod.org/#/c/129433/
    
    Change-Id: I18a896ac082d7c82d1864eb4369c0a7c5aa78501

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -765,7 +765,7 @@ public abstract class PanelView extends FrameLayout {
             mUpdateExpandOnLayout = isFullyCollapsed();
             mFlingAnimationUtils.apply(animator, mExpandedHeight, target, vel, getHeight());
             if (vel == 0) {
-                animator.setDuration(350);
+                animator.setDuration(400);
             }
         } else {
             if (shouldUseDismissingAnimation()) {
