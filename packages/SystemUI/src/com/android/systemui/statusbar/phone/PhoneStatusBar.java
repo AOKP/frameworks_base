01817e0b28dfa50458e5d6aa473d5654822ce83f
Roman Birg <roman@cyngn.com>
2016-03-17 09:18:56 -0700

    SystemUI: more consistent visualizer logic
    - only call setVisible from screen on and screen off methods
    - eagerly unregister self to make sure we don't get added twice as a
      listener
    - Add some debugging under the DEBUG flag
    
    Change-Id: Iada13058f87d4c8d8c8b60f6eebf055652ff2c8d
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2377,7 +2377,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         if (!mKeyguardFadingAway && keyguardVisible && backdropBitmap != null && mScreenOn) {
             // if there's album art, ensure visualizer is visible
-            mVisualizerView.setVisible(true);
             mVisualizerView.setPlaying(mMediaController != null
                     && mMediaController.getPlaybackState() != null
                     && mMediaController.getPlaybackState().getState()
@@ -4720,7 +4719,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         - StatusBarIconController.DEFAULT_TINT_ANIMATION_DURATION,
                 StatusBarIconController.DEFAULT_TINT_ANIMATION_DURATION);
         disable(mDisabledUnmodified1, mDisabledUnmodified2, fadeoutDuration > 0 /* animate */);
-        mVisualizerView.setVisible(false);
     }
 
     public boolean isKeyguardFadingAway() {
@@ -5081,7 +5079,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mWakeUpTouchLocation = null;
         mStackScroller.setAnimationsEnabled(false);
         updateVisibleToUser();
-        mVisualizerView.setVisible(false);
         if (mQSTileHost.isEditing()) {
             mQSTileHost.setEditing(false);
         }
@@ -5130,6 +5127,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     public void onScreenTurnedOff() {
+        mVisualizerView.setVisible(false);
         if (mNotificationPanel.hasExternalKeyguardView()) {
             mNotificationPanel.getExternalKeyguardView().onScreenTurnedOff();
         }
