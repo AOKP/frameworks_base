3688356a1aad98231318c9334779b93197cec3d1
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    Fw_base - HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: Ib836eb51f55e4b3853a108182204cc4aef7f59fd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4310,6 +4310,12 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
