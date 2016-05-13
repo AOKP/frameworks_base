dbb90ab5b31c73121f16d301df96e2d2f16f4838
STELIX <ssspinni@gmail.com>
2016-05-13 10:58:48 +0200

    Revert "Fw_base - Statusbar weather color (1/2)"
    This reverts commit 7221521e5545baf6465a2cbe524b026a8a1700f7.
    
    Change-Id: I7e7f1ed673ba4fff0d196554e1c9db5fece5e939
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4069,12 +4069,6 @@ public final class Settings {
         public static final String STATUS_BAR_WEATHER_TEMP_STYLE = "status_bar_weather_temp_style";
 
         /**
-         * Statusbar weather color setting
-         * @hide
-         */
-        public static final String STATUS_BAR_WEATHER_COLOR = "status_bar_weather_color";
-
-        /**
          * Status bar carrier label
          * 0: Hide
          * 1: Display on keyguard status bar
