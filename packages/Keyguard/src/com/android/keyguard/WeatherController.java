853b4e43ac021e57c215c41802afa46799fe2cc1
STELIX <ssspinni@gmail.com>
2016-05-17 17:17:03 +0200

    Revert "Revert "Fw_base - Lockscreen weather options [1/2]""
    This partially reverts commit 7e75405b08729408a66cf37b97fb13509abdff82.
    We'll keep only lockscreen weather changes.
    
    Change-Id: Ibe912a96f6581870568111c57881a2fb1e4dcd48
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- packages/Keyguard/src/com/android/keyguard/WeatherController.java
+++ packages/Keyguard/src/com/android/keyguard/WeatherController.java
@@ -14,26 +14,28 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.aokp;
+package com.android.keyguard;
 
 import android.graphics.drawable.Drawable;
 
 public interface WeatherController {
     void addCallback(Callback callback);
     void removeCallback(Callback callback);
-    void updateWeather();
     WeatherInfo getWeatherInfo();
+    void updateWeather();
 
     public interface Callback {
         void onWeatherChanged(WeatherInfo temp);
     }
     public static class WeatherInfo {
         public String city = null;
-        public String wind = null;
+        public double windSpeed = Double.NaN;
         public int conditionCode = 0;
         public Drawable conditionDrawable = null;
-        public String temp = null;
+        public double temp = Double.NaN;
         public String humidity = null;
         public String condition = null;
+        public int tempUnit;
+        public int windSpeedUnit;
     }
 }
