a662b8c15ed4dcedfe7f8fb0994bf04ec1bffc54
Raleigh Matlock <raleighman2@gmail.com>
2016-05-27 14:37:45 +0200

    Fw_base - BatteryService: Fully charged notification toggle (1/2)
    Change-Id: I269606e054899e822d98e72db75e4ac3bacddc43
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4523,6 +4523,12 @@ public final class Settings {
         public static final String PA_PIE_ALWAYS_RIGHT = "pa_pie_always_right";
 
         /**
+         * Whether to show the battery fully charged notification
+         * @hide
+         */
+        public static final String BATTERY_FULLY_CHARGED_NOTIFICATION = "battery_fully_charged_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
