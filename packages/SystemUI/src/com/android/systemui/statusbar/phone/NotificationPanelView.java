8f0ca48e5529bfa8794c9394617b32901dad51b5
d34d <clark@cyngn.com>
2016-07-25 09:21:26 -0700

    SysUI: Define config for showing screen on hints
    If you want it, enable the config otherwise stay calm and carry on.
    
    Change-Id: I057443dc289098dad21775f941e13903881c6832

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -297,6 +297,8 @@ public class NotificationPanelView extends PanelView implements
     private boolean mUserInteractedWithLiveLockScreen;
     private boolean mUserReturnedFromLiveLockScreen;
 
+    private boolean mScreenOnHintsEnabled;
+
     private enum SwipeLockedDirection {
         UNKNOWN,
         HORIZONTAL,
@@ -440,6 +442,7 @@ public class NotificationPanelView extends PanelView implements
         mScreenHeight = point.y;
         mUnlockMethodCache = UnlockMethodCache.getInstance(context);
 
+        mScreenOnHintsEnabled = res.getBoolean(R.bool.config_showScreenOnLockScreenHints);
         mUserUnlocked = getUserUnlocked();
         mUserExpandedNotifications = getUserExpandedNotificationsInKeyguard();
         mUserInteractedWithLiveLockScreen = getUserInteractedWithLls();
@@ -3155,6 +3158,7 @@ public class NotificationPanelView extends PanelView implements
     }
 
     private boolean shouldShowScreenOnHints() {
-        return mStatusBar.isDeviceProvisioned() && mStatusBarState == StatusBarState.KEYGUARD;
+        return mScreenOnHintsEnabled && mStatusBar.isDeviceProvisioned() &&
+                mStatusBarState == StatusBarState.KEYGUARD;
     }
 }
