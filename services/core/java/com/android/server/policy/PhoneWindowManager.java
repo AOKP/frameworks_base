4dd0cd5ce8ae38048fc758defdd04518b59eabfe
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2016-08-01 10:36:52 +0200

    Revert "Fw_base - Bootmsg: The app name is now displayed in bold text."
    This reverts commit 2a51deb8c6271c215459b5c510acb777a7b75859.
    
    Change-Id: I6f6412d4b541401c83aab60d6328794548b93d18

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7570,7 +7570,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (always && (currentPackageName != null)) {
                     // Only display the current package name if the main message says "Optimizing app N of M".
                     // We don't want to do this when the message says "Starting apps" or "Finishing boot", etc.
-                    mBootMsgDialog.setMessage(Html.fromHtml(msg + "<br><b>" + currentPackageName + "</b>"));
+                    mBootMsgDialog.setMessage(msg + "\n" + currentPackageName);
                 }
                 else {
                     mBootMsgDialog.setMessage(msg);
