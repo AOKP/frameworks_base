319397179610c5312591dce31c78e88dc4577bc7
Alexander Martinz <eviscerationls@gmail.com>
2016-09-30 20:22:21 +0200

    Fw_base - Add Haptic Feedback to QS tiles [1/2]
    Change-Id: If358fb624032cd24d98fe2ea0c877f4875c5c151

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3808,6 +3808,13 @@ public final class Settings {
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
         /**
+         * Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
+        /**
          * Toast animations
          *
          * @hide
