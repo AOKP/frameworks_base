d9950a2947d2e4e70b018ac995c1da8dc251ea04
Nico60 <ndhouailly@gmail.com>
2016-10-01 10:36:17 +0200

    Fw_base - Hide arrows in network traffic indicators [1/2]
    AICPfy:
    PS2:
    - Add back line for setting color filter
    
    Change-Id: I846c1091d2f88f27f26bf2e9f8e2ac2c6bd67a07

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3781,6 +3781,12 @@ public final class Settings {
         public static final String BRIGHTNESS_ICON = "brightness_icon";
 
         /**
+         * Whether to disable showing arrows in network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
