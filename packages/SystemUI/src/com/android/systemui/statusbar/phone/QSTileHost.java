2e977672a5b190056bd9b8c83fd39c6a7d5f2adc
Anas Karbila <anaskarbila@aospa.co>
2016-12-11 00:55:02 +0100

    SystemUI: Add NFC tile
    This patch adds a tile for QuickSettings
    which controls the setting of NFC.
    
    The drawable is from material.io.
    
    Test: Enable and disable NFC using tile
    
    Change-Id: I7dfaf3a4dca0c01d24be397e43ab131eb9885f62
    Signed-off-by: Anas Karbila <anaskarbila@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -63,6 +63,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NavigationBarTile;
+import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.PieTile;
 import com.android.systemui.qs.tiles.PowerMenuTile;
@@ -477,6 +478,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("gesture_anywhere")) return new GestureAnyWhereTile(this);
         else if (tileSpec.equals("volume")) return new VolumeTile(this);
         else if (tileSpec.equals("pie")) return new PieTile(this);
+        else if (tileSpec.equals("nfc")) return new NfcTile(this);
 
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
