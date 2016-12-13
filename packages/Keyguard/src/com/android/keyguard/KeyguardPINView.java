7482ecdc8594ced75c11d3ba2062df90fcdd3cfa
Stefano Mura [STELIX PippoX3] <ssspinni@gmail.com>
2016-12-13 21:33:08 +0100

    Revert "Fw_base - Keyguard: Forward port lockscreen quick unlock (1/2)"
    This reverts commit a38170b3eff306095a7afe6730c5d66d9d075237.
    
    Change-Id: Iff5c2bb1c13807ffa0da93163bb6ec1e4406f9a6

--- packages/Keyguard/src/com/android/keyguard/KeyguardPINView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardPINView.java
@@ -29,8 +29,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import com.android.internal.widget.LockPatternUtils.RequestThrottledException;
-import com.android.keyguard.PasswordTextView.QuickUnlockListener;
 import com.android.settingslib.animation.AppearAnimationUtils;
 import com.android.settingslib.animation.DisappearAnimationUtils;
 
@@ -55,8 +53,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
 
     private static List<Integer> sNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-
-    private final int userId = KeyguardUpdateMonitor.getCurrentUser();
 
     public KeyguardPINView(Context context) {
         this(context, null);
@@ -126,9 +122,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
         boolean scramblePin = (CMSettings.System.getInt(getContext().getContentResolver(),
                 CMSettings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0) == 1);
 
-        boolean quickUnlock = (Settings.System.getInt(getContext().getContentResolver(),
-                Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0) == 1);
-
         if (scramblePin) {
             Collections.shuffle(sNumbers);
             // get all children who are NumPadKey's
@@ -151,16 +144,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
                 NumPadKey view = views.get(i);
                 view.setDigit(sNumbers.get(i));
             }
-        }
-
-        if (quickUnlock) {
-            mPasswordEntry.setQuickUnlockListener(new QuickUnlockListener() {
-                public void onValidateQuickUnlock(String password) {
-                    validateQuickUnlock(password);
-                }
-            });
-        } else {
-            mPasswordEntry.setQuickUnlockListener(null);
         }
     }
 
@@ -224,24 +207,5 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     @Override
     public boolean hasOverlappingRendering() {
         return false;
-    }
-
-    private void validateQuickUnlock(String password) {
-        if (password != null) {
-            if (password.length() > MINIMUM_PASSWORD_LENGTH_BEFORE_REPORT
-                    && kpvCheckPassword(password)) {
-                mCallback.reportUnlockAttempt(userId, true, 0);
-                mCallback.dismiss(true);
-                resetPasswordText(true, true);
-            }
-        }
-    }
-
-    private boolean kpvCheckPassword(String password) {
-        try {
-            return mLockPatternUtils.checkPassword(password, userId);
-        } catch (RequestThrottledException ex) {
-            return false;
-        }
     }
 }
