04113d9d31548e0e6509f8deb8a95631010813f6
Lars Greiss <kufikugel@googlemail.com>
2016-12-19 14:52:30 +0100

    Fw_base - add back all keyboard IME features back (1/2)
    this commit adds:
    
    - ime switcher notification - thanks cm
    - slims fullscreen disable on landscape
    - slims force autorotation on IME
    - volume button control for navigate in the message - thanks cm
    - slims force to show always enter key
    
    some fixes to the 4.2/4.3 implementations + added full multiuser
    support for all this features
    
    cheers
    
    RR:
    Removed the following as we are using CM's version
    - ime switcher
    - volume button control for navigate in the message
    
    AICPfy:
    - Brought back to N from AICP MM by @LorDClockaN
    - Fix update API message
    
    Change-Id: I71e93c39620cc171e658b2d7e26491bac97c651d

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -78,6 +78,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_DISMISS_KEYBOARD_SHORTCUTS    = 32 << MSG_SHIFT;
     private static final int MSG_SCREEN_PINNING_STATE_CHANGED  = 33 << MSG_SHIFT;
     private static final int MSG_HANDLE_SYSNAV_KEY             = 34 << MSG_SHIFT;
+    private static final int MSG_SET_AUTOROTATE_STATUS         = 35 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -133,6 +134,7 @@ public class CommandQueue extends IStatusBar.Stub {
         void remQsTile(ComponentName tile);
         void clickTile(ComponentName tile);
         void screenPinningStateChanged(boolean enabled);
+        void setAutoRotate(boolean enabled);
 
         void handleSystemNavigationKey(int arg1);
     }
@@ -409,6 +411,14 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void setAutoRotate(boolean enabled) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_SET_AUTOROTATE_STATUS);
+            mHandler.obtainMessage(MSG_SET_AUTOROTATE_STATUS,
+                enabled ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         public void handleMessage(Message msg) {
             final int what = msg.what & MSG_MASK;
@@ -526,6 +536,9 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;
                 case MSG_SCREEN_PINNING_STATE_CHANGED:
                     mCallbacks.screenPinningStateChanged(msg.arg1 != 0);
+                    break;
+                case MSG_SET_AUTOROTATE_STATUS:
+                    mCallbacks.setAutoRotate(msg.arg1 != 0);
                     break;
                 case MSG_HANDLE_SYSNAV_KEY:
                     mCallbacks.handleSystemNavigationKey(msg.arg1);
--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -99,6 +99,10 @@ public class TvStatusBar extends BaseStatusBar {
     }
 
     @Override
+    public void setAutoRotate(boolean enabled) {
+    }
+
+    @Override
     protected void setAreThereNotifications() {
     }
 
