7757e390952535af07549eed22327ecefd26f283
STELIX <ssspinni@gmail.com>
2016-12-22 20:40:07 +0100

    Fw_base - PhoneStatusBar: change ContentObserver
    Change-Id: I5a4aa63c866936b2d10af52eb50b996c16b1e681
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -560,6 +560,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         void observe() {
             ContentResolver resolver = mContext.getContentResolver();
+
             resolver.registerContentObserver(LineageSettings.Global.getUriFor(
                     LineageSettings.Global.DEV_FORCE_SHOW_NAVBAR), false, this,
                     UserHandle.USER_ALL);
@@ -985,8 +986,8 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
 
         createAndAddWindows();
-
         mSettingsObserver.onChange(false); // set up
+        mUserSettingsObserver.observe();
         mCommandQueue.disable(switches[0], switches[6], false /* animate */);
         setSystemUiVisibility(switches[1], switches[7], switches[8], 0xffffffff,
                 fullscreenStackBounds, dockedStackBounds);
@@ -6156,6 +6157,29 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateNotifications();
         }
     };
+
+    private UserSettingsObserver mUserSettingsObserver = new UserSettingsObserver(mHandler);
+    private class UserSettingsObserver extends ContentObserver {
+        UserSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange);
+            ContentResolver resolver = mContext.getContentResolver();
+        }
+
+        public void update() {
+
+        }
+
+    }
 
     private RemoteViews.OnClickHandler mOnClickHandler = new RemoteViews.OnClickHandler() {
 
