c3f1a67c135d131493d67ba764cd7cda97c3e505
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-01-14 12:54:00 -0500

    Fw_base - DUI: double tap to sleep on navbar [1/3]
    this is more reliable than double tap to sleep action on
    navbar buttons
    
    Change-Id: Ibd1d68914a064b50000673b944ec94bfa735a19e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4764,6 +4764,12 @@ public final class Settings {
         public static final String QS_QUICKBAR_SCROLL_ENABLED = "qs_quickbar_scroll_enabled";
 
         /**
+         *  Enable smartbar double tap to sleep gesture
+         * @hide
+         */
+        public static final String SMARTBAR_DOUBLETAP_SLEEP = "smartbar_doubletap_sleep";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
