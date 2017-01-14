86f076ee101f7c5a13d0bbca6d089f650f197bb9
xyyx <xyyx@mail.ru>
2017-01-14 14:31:18 +0800

    Ability to disable Do not Disturb mode when calling (1/3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5023,6 +5023,12 @@ public final class Settings {
         public static final String DASHBOARD_LANDSCAPE_COLUMNS = "dashboard_landscape_columns";
 
         /**
+         * Use Dnd when call active
+         * @hide
+         */
+        public static final String DND_WHEN_CALL = "dnd_when_call";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
