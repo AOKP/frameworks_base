14c1a932d3cf3a5ce4e77b59170d07a1eab32260
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-26 16:19:19 -0600

    core: more descriptive vendor image mismatch message
    * On lineage, we don't update users' vendor images. This can
      cause an issue when the fingerprint of the rom doesn't match
      the fingerprint on the vendor image. Let's use a more descriptive
      message to tell the user what needs to be done, rather than telling
      them to contact their manufacturer.
    
    Change-Id: I8ffd0fb7347454a52aa71498719a407447e6e8f4

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1716,7 +1716,8 @@ public final class ActivityManagerService extends ActivityManagerNative
                     d.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ERROR);
                     d.setCancelable(false);
                     d.setTitle(mContext.getText(R.string.android_system_label));
-                    d.setMessage(mContext.getText(R.string.system_error_vendorprint));
+                    d.setMessage(mContext.getString(R.string.aokp_system_error_manufacturer,
+                            SystemProperties.get("ro.build.fingerprint").split("/")[3]));
                     d.setButton(DialogInterface.BUTTON_POSITIVE, mContext.getText(R.string.ok),
                             obtainMessage(DISMISS_DIALOG_UI_MSG, d));
                     d.show();
