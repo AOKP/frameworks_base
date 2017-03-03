3d12d266cd3256b93a3d66890aa5b51359c0105e
Nathan Chancellor <natechancellor@gmail.com>
2017-03-04 00:35:08 +0100

    Revert "Add a protected broadcast for Masquerade events"
    Requires https://substratum.review/#/c/374
    
    This reverts commit 4cd8990c89e79acb926f9c1ae1959661955dcbe2.
    
    Change-Id: Ida416c3949379a3d6a24a104667c74eef1f56630

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4566,8 +4566,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         || actionName.startsWith("com.android.server.sip.SipWakeupTimer")
                         || actionName.startsWith("com.android.internal.telephony.data-reconnect")
                         || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")
-                        || actionName.startsWith("intent_navbar_edit")
-                        || actionName.startsWith("masquerade.substratum.STATUS_CHANGED")) {
+                        || actionName.startsWith("intent_navbar_edit")) {
                     return true;
                 }
             }
