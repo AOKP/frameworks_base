166dc061c9aaab9b5aa5a2e2e3c31ef7485c3c9c
Varun Date <date.varun123@gmail.com>
2017-03-24 18:45:05 +0100

    Fw_base - SmartBar Button Tint [1/3]
    With Reference to Patch AICP/frameworks_base@85d97f4
    
    thanks to lichti1901 for this initial Patch
    
    Patchset 2 : Fix Back Button Tinting
    Thanks to  @LorDClockaN
    
    Based on this patch
    
    AICP/frameworks_base@d09e1cc
    
    Patchset 3 : Fix New Actions FCs
    
    Change-Id: Iee96af0cdb3d119b0b422fe1fcb27ddbf30c05a5
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4863,6 +4863,18 @@ public final class Settings {
                 "notification_guts_kill_app_button";
 
         /**
+         * Navbar Button Color Switch
+         * @hide
+         */
+        public static final String NAVBAR_TINT_SWITCH = "navbar_tint_switch";
+
+         /**
+         * Navbar Button Color
+         * @hide
+         */
+        public static final String NAVBAR_BUTTON_COLOR= "navbar_button_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
