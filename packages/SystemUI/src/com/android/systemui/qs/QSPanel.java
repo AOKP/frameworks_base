e85b43afdcc0b91b38da00d8d794c1b9de6f6ae5
Michael Bestas <mkbestas@lineageos.org>
2017-04-30 13:45:39 +0300

    SystemUI: Add tunable for auto brightness icon
    Change-Id: I02fe3f5500f95ba9369b952bbec2a073b7ae8316

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -59,14 +59,20 @@ import com.android.systemui.tuner.TunerService.Tunable;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import cyanogenmod.providers.CMSettings;
+
 /** View that represents the quick settings tile panel. **/
 public class QSPanel extends LinearLayout implements Tunable, Callback {
 
-    public static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
+    public static final String QS_SHOW_BRIGHTNESS_SLIDER =
+            "cmsecure:" + CMSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER;
+    public static final String QS_SHOW_AUTO_BRIGHTNESS =
+            "cmsecure:" + CMSettings.Secure.QS_SHOW_AUTO_BRIGHTNESS;
 
     protected final Context mContext;
     protected final ArrayList<TileRecord> mRecords = new ArrayList<TileRecord>();
     protected final View mBrightnessView;
+    protected final ImageView mAutoBrightnessView;
     protected final ImageView mBrightnessIcon;
     private final H mHandler = new H();
 
@@ -74,6 +80,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
     private int mBrightnessPaddingTop;
     protected boolean mExpanded;
     protected boolean mListening;
+    private boolean mIsAutomaticBrightnessAvailable = false;
 
     private Callback mCallback;
     private BrightnessController mBrightnessController;
@@ -119,6 +126,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
                 mBrightnessIcon,
                 (ToggleSlider) findViewById(R.id.brightness_slider));
 
+        mAutoBrightnessView = (ImageView) findViewById(R.id.brightness_icon);
     }
 
     protected void setupTileLayout() {
@@ -126,10 +134,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
                 R.layout.qs_paged_tile_layout, this, false);
         mTileLayout.setListening(mListening);
         addView((View) mTileLayout);
-        if (getResources().getBoolean(
-                com.android.internal.R.bool.config_automatic_brightness_available)) {
-            ((ImageView) findViewById(R.id.brightness_icon)).setVisibility(View.VISIBLE);
-        }
+
+        mIsAutomaticBrightnessAvailable = getResources().getBoolean(
+                com.android.internal.R.bool.config_automatic_brightness_available);
     }
 
     public boolean isShowingCustomize() {
@@ -139,7 +146,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        TunerService.get(mContext).addTunable(this, QS_SHOW_BRIGHTNESS);
+        TunerService.get(mContext).addTunable(this,
+                QS_SHOW_BRIGHTNESS_SLIDER,
+                QS_SHOW_AUTO_BRIGHTNESS);
         if (mHost != null) {
             setTiles(mHost.getTiles());
         }
@@ -162,8 +171,11 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        if (QS_SHOW_BRIGHTNESS.equals(key)) {
+        if (QS_SHOW_BRIGHTNESS_SLIDER.equals(key)) {
             mBrightnessView.setVisibility(newValue == null || Integer.parseInt(newValue) != 0
+                    ? VISIBLE : GONE);
+        } else if (QS_SHOW_AUTO_BRIGHTNESS.equals(key) && mIsAutomaticBrightnessAvailable) {
+            mAutoBrightnessView.setVisibility(newValue == null || Integer.parseInt(newValue) != 0
                     ? VISIBLE : GONE);
         }
     }
