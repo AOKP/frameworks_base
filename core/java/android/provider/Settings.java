1e932d3ca03e7d0c299a9c05e928bb14a6560187
Varun Date <date.varun123@gmail.com>
2017-05-04 13:25:00 +0300

    Fw_base - SystemUI: Keyguard weather (1/2)
    TODO: add max # of notifications check to properly hide weather panel
    
    AICP changes:
    Brought to O
    
    Change-Id: I3efd56c92865085bfd1e0685a14dc14a83031437
    Signed-off-by: Ali B <abittin@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4520,6 +4520,24 @@ public final class Settings {
         public static final String SCREEN_STATE_ON_DELAY = "screen_state_on_delay";
 
         /**
+         * Whether to show the weather info on the lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER = "lock_screen_show_weather";
+
+        /**
+         * Whether to show the weather location on lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_WEATHER_LOCATION = "lock_screen_show_weather_location";
+
+        /**
+         * Whether to show the weather condition icon on lock screen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_WEATHER_CONDITION_ICON = "lock_screen_weather_condition_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
