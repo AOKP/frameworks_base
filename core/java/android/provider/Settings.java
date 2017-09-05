6d184a7acd618c5a7eb95fffc332ede9a16527a4
maxwen <max.weninger@gmail.com>
2017-09-05 20:36:26 +0200

    Fw_base - SystemUI: add CPU info overlay
    Change-Id: I3c75a9c2837c9293822a33a95380cc1edb7f2b1a
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: villarleg <santialovera20@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12587,6 +12587,11 @@ public final class Settings {
         public static final String SCREEN_ON_ANIMATION = "screen_on_animation";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
