36cc2e7e64f9f8a088717620ade0bf95d1aaa145
cphelps76 <cphelps76@droidconcepts.com>
2017-10-03 20:02:52 +0200

    [SQUASH]BatteryBar [1/2]
    base: Add back battery bar [1/2]
    {x} Adds Battery bar with following features:
    {x} aligment
    {x} thickness
    {x} charging animation
    
    Battery bar enhancements [1/2]
    
    Add color settings for charging & low battery
    Add reversed alignment setting
    
    Battery Bar Gradient color [1/2]
    
    Change-Id: I62ed7b7c03c1a563caa02498d23fc08da9ea11cf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3982,7 +3982,64 @@ public final class Settings {
         };
 
         /**
-         * Setting to determine whether or not to show the battery percentage in the status bar.
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String BATTERY_BAR_LOCATION = "battery_bar_location";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_COLOR = "battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_THICKNESS = "battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_STYLE = "battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_ANIMATE = "battery_bar_animate";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_CHARGING_COLOR = "battery_bar_charging_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_BATTERY_LOW_COLOR_WARNING = "battery_bar_battery_low_color_warning";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_HIGH_COLOR = "battery_bar_high_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_LOW_COLOR = "battery_bar_low_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_USE_GRADIENT_COLOR = "battery_bar_use_gradient_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_BATTERY_LOW_COLOR = "battery_bar_battery_low_color";
+
+
+	/**
+	 * Setting to determine whether or not to show the battery percentage in the status bar.
          *    0 - Don't show percentage
          *    1 - Show percentage
          * @hide
