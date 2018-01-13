844127417132ad0153389ce3d23ac930f0d7d5d6
SpiritCroc <spiritcroc@gmail.com>
2018-01-13 12:16:46 +0100

    Fw_base - Add Always On Display QS Tile
    @neobuddy89: Simplify and fix multiuser setting.
    
    Change-Id: Ic284485b42da25374191bd7a40419fa605caf842
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: villarleg <santialovera20@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -26,6 +26,7 @@ import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AlwaysOnDisplayTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
@@ -152,6 +153,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new PieTile(mHost);
             case "weather":
                 return new WeatherTile(mHost);
+            case "always_on_display":
+                return new AlwaysOnDisplayTile(mHost);
         }
         
         // Intent tiles.
