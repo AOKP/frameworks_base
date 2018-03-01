0b2e5a114795f822bbbb3dbb210ff40ef200605a
Daniel Koman <dankoman30@gmail.com>
2018-03-01 15:10:46 +0100

    Fw_base - Add kill button to notification guts [1/2]
    [SpiritCroc]
    - Make it a setting
    - Don't allow killing apps when device is locked
    - Bring it to Oreo
    
    Change-Id: I7c18b85a952c57fdb3df36a69c069d247e65e643

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4340,6 +4340,14 @@ public final class Settings {
         public static final String QS_HEADER_BATTERY_PERCENT = "qs_header_force_battery_percent";
 
         /**
+         * Whether to show the kill app button in notification guts
+         *
+         * @hide
+         */
+        public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
+                "notification_guts_kill_app_button";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
