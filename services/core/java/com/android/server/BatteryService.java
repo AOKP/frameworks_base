eac3f78782d7fdfebc5a5f6d7d5f93ff4d7e165c
Sam Mortimer <sam@mortimer.me.uk>
2018-03-08 23:32:25 +0100

    frameworks/base: disable warning about mLineageBatteryLights not being ready *) Happens during normal boot (until we reach PHASE_BOOT_COMPLETED).
    *) Only show the warning if DEBUG is set.
    Change-Id: I4663858358e143742bfb9cc4da71df0dc0e53c02

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -927,7 +927,8 @@ public final class BatteryService extends SystemService {
         public void updateLightsLocked() {
             // mBatteryProps could be null on startup (called by SettingsObserver)
             if (mBatteryProps == null) {
-                Slog.w(TAG, "updateLightsLocked: mBatteryProps is null; skipping");
+              if (DEBUG){
+                Slog.w(TAG, "updateLightsLocked: mBatteryProps is null; skipping");}
                 return;
             }
             // mLineageBatteryLights is initialized during PHASE_BOOT_COMPLETED
