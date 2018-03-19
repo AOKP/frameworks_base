8509d1f84e76ca6e57c490fedf732ae61d327b53
Varun Date <date.varun123@gmail.com>
2018-03-19 14:05:19 +0100

    Fw_base - SmartBar Button Tint [1/3]
    Change-Id: I1d5926338843ede8ced30ab2f2bd3315024563b6
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4195,6 +4195,18 @@ public final class Settings {
         public static final String NAVBAR_DYNAMIC = "navbar_dynamic";
 
         /**
+         * Navbar Button Color Switch
+         * @hide
+         */
+        public static final String NAVBAR_TINT_SWITCH = "navbar_tint_switch";
+
+         /**
+         * Navbar Button Color
+         * @hide
+         */
+        public static final String NAVBAR_BUTTON_COLOR= "navbar_button_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
