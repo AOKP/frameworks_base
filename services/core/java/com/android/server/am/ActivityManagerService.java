5bc9d2fd09cb8a3bd69ed2883343a9c5a3cbc704
Jesse Chan <jc@lineageos.org>
2018-04-21 23:45:57 -0700

    Add an option to force pre-O apps to use full screen aspect ratio
    When an app target pre-O releases, the default max aspect ratio
    is 1.86:1 which leads to ugly black areas on devices that have
    screens with higher aspect ratio (for example Galaxy S8/S9).
    
    This change adds an option to allow users to change aspect ratio
    for pre-O apps to full screen aspect ratio.
    
    Change-Id: I41d5408841593a12443be885e11959bffaebb67b

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -414,6 +414,8 @@ import com.android.server.vr.VrManagerInternal;
 import com.android.server.wm.PinnedStackWindowController;
 import com.android.server.wm.WindowManagerService;
 
+import org.lineageos.internal.applications.LineageActivityManager;
+
 import java.text.SimpleDateFormat;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -1741,6 +1743,9 @@ public class ActivityManagerService extends IActivityManager.Stub
     final boolean mPermissionReviewRequired;
 
     private static String sTheRealBuildSerial = Build.UNKNOWN;
+
+    // Lineage sdk activity related helper
+    private LineageActivityManager mLineageActivityManager;
 
     /**
      * Current global configuration information. Contains general settings for the entire system,
@@ -12233,6 +12238,10 @@ public class ActivityManagerService extends IActivityManager.Stub
         RescueParty.onSettingsProviderPublished(mContext);
 
         //mUsageStatsService.monitorPackages();
+
+        // LineageActivityManager depends on settings so we can initialize only
+        // after providers are available.
+        mLineageActivityManager = new LineageActivityManager(mContext);
     }
 
     private void startPersistentApps(int matchFlags) {
@@ -24786,5 +24795,9 @@ public class ActivityManagerService extends IActivityManager.Stub
                 Binder.restoreCallingIdentity(origId);
             }
         }
+    }
+
+    public boolean shouldForceLongScreen(String packageName) {
+        return mLineageActivityManager.shouldForceLongScreen(packageName);
     }
 }
