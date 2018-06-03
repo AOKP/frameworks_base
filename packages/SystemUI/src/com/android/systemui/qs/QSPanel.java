108b268e166fa37c06c28d4b0839a0cbbdb4749d
Umang96 <umangleekha3@gmail.com>
2018-06-03 13:05:39 +0530

    [BACKPORT] SystemUI: Fix alignment glitch with brightness mirror
    Alignment of brightness mirror dialog was not exatly same as the
    QS brightness dialog, which produced a visual glitch of slider
    thumb shifting whenever we change brightness.
    This is solved by dynamically handling the visibility of
    mirror brightness icon, gone when brightness icon is off and
    invisible when brightness icon is on, so that both mirror and
    QS slider are equally aligned.
    
    Change-Id: Ifb85643db669ee8d7473d3c48e7e4733428a77d6

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -95,6 +95,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
     private Record mDetailRecord;
 
     private BrightnessMirrorController mBrightnessMirrorController;
+    private ImageView mMirrorAutoBrightnessView;
 
     protected Vibrator mVibrator;
 
@@ -177,6 +178,10 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
         } else if (QS_SHOW_AUTO_BRIGHTNESS.equals(key) && mIsAutomaticBrightnessAvailable) {
             mAutoBrightnessView.setVisibility(newValue == null || Integer.parseInt(newValue) != 0
                     ? VISIBLE : GONE);
+            if (mMirrorAutoBrightnessView != null) {
+                mMirrorAutoBrightnessView.setVisibility(newValue == null ||
+                        Integer.parseInt(newValue) != 0 ? INVISIBLE : GONE);
+            }
         }
     }
 
@@ -206,6 +211,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
         mBrightnessMirrorController = c;
         ToggleSlider brightnessSlider = (ToggleSlider) findViewById(R.id.brightness_slider);
         ToggleSlider mirror = (ToggleSlider) c.getMirror().findViewById(R.id.brightness_slider);
+        mMirrorAutoBrightnessView = (ImageView) c.getMirror().findViewById(R.id.brightness_icon);
+        mMirrorAutoBrightnessView.setVisibility(mAutoBrightnessView.getVisibility()
+                    == View.VISIBLE ? View.INVISIBLE : View.GONE);
         brightnessSlider.setMirror(mirror);
         brightnessSlider.setMirrorController(c);
     }
