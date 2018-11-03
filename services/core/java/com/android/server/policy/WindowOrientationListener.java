eb2582a2f9bcc3944ea044a71987af8a7b09693b
villarleg <santialovera20@gmail.com>
2018-11-03 14:04:08 +0100

    Revert "policy: add more orientation sanity checks"
    This reverts commit 64155c037e09456bb9c9dca64af772175d29175e.

--- services/core/java/com/android/server/policy/WindowOrientationListener.java
+++ services/core/java/com/android/server/policy/WindowOrientationListener.java
@@ -1084,10 +1084,6 @@ public abstract class WindowOrientationListener {
 
         public int evaluateRotationChangeLocked() {
             unscheduleRotationEvaluationLocked();
-            // maxwen: op6_sensor_tweak
-            if (mDesiredRotation > 3) {
-                return -1;
-            }
             if (mDesiredRotation == mProposedRotation) {
                 return -1;
             }
